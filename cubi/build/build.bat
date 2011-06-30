a362ad4deace05fc3bc8976ecfff308d203e6480
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-06-30 00:26:06 +0000

    fix build scripts
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@24 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/build/build.bat
+++ cubi/build/build.bat
@@ -4,7 +4,7 @@ rem *********************************************************************
 rem cubi builder script. Usage: build app_name
 rem *********************************************************************
 
-set PHING_HOME=C:\xampp\htdocs\ob3\cubi\bin\phing
+set PHING_HOME=C:\xampp\htdocs\gcubi\cubi\bin\phing
 set PHP_CLASSPATH=%PHING_HOME%\classes
 
 %PHING_HOME%\bin\phing -buildfile %1.xml %2
