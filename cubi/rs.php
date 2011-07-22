a8dee7ec31873b35cfb16ef9138bcb32cfbf162c
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-07-22 08:50:10 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@164 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/rs.php
+++ cubi/rs.php
@@ -6,11 +6,12 @@
 //include_once "bin/app_init.php";
 define('APP_HOME',dirname(__FILE__));
 define('RESOURCE_PATH', APP_HOME.DIRECTORY_SEPARATOR."resources");
+define('CACHE_DATA_PATH', APP_HOME.DIRECTORY_SEPARATOR."files".DIRECTORY_SEPARATOR."cache".DIRECTORY_SEPARATOR."data");
 
 $file = $_GET['f'];
 // get extention of given file
 $list = explode('.',$file);
-$ext = $list[count($list)];
+$ext = $list[count($list)-1];
 if ($ext == 'jpg' || $ext == 'gif' || $ext == 'png' || $ext == 'swf') {
     include "img.php";
 }
