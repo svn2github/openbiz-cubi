80dc0f74a4138d53128d6d2ad2595a4c396348c4
jim.jenkins.iv@gmail.com <jim.jenkins.iv@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-08-07 02:34:14 +0000

    Update copyright Test svn:keywords
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@222 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/bin/easy/EasyFormGrouping.php
+++ cubi/openbiz/bin/easy/EasyFormGrouping.php
@@ -1,4 +1,20 @@
 <?php 
+
+/**
+ * PHPOpenBiz Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @package   openbiz.bin.easy
+ * @copyright Copyright (c) 2005-2011, Rocky Swen
+ * @license   http://www.opensource.org/licenses/bsd-license.php
+ * @link      http://www.phpopenbiz.org/
+ * @version   $Id$
+ */
+
 class EasyFormGrouping extends EasyForm
 {
 	protected $m_GroupBy;
