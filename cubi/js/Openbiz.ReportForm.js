1a8c299a3fd6a6538696c3edb9ff88cd28032d72
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-09-15 06:14:45 +0000

    add PivotForm js class
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@309 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/js/Openbiz.ReportForm.js
+++ cubi/js/Openbiz.ReportForm.js
@@ -10,11 +10,6 @@ Openbiz.ReportForm = Class.create(Openbiz.TableForm,
         this.baseFormName = formNameArr[0];
         this.reportFormName = formNameArr[1];
     },
-    renderPivot: function(paramArray, options)
-    {
-        if (validatePivotForm())
-            this.CallFunction("renderPivot", paramArray, options);
-    },
 	CallFunction: function(method, paramArray, options)
     {
         Openbiz.activeForm = this;
@@ -51,3 +46,41 @@ Openbiz.ReportForm = Class.create(Openbiz.TableForm,
         }
     }
 });
+
+/**
+ * Openbiz Pivot Form class
+ */
+Openbiz.PivotForm = Class.create(Openbiz.Form,
+{
+	initialize: function($super, name, subForms)
+    {
+        $super(name, subForms);
+	},
+    renderPivot: function(paramArray, options)
+    {
+        if (validatePivotForm()) {
+			this.form.setAttribute("target", "_blank");
+            this.CallFunction("renderPivot", paramArray, options);
+		}
+    }
+});
+
+// validation of input
+var pivotInputs = ['fld_colfld1','fld_colfld2','fld_rowfld1','fld_rowfld2','fld_rowfld3','fld_datafld1'];
+function validatePivotInputs(elem) {
+    for(i=0; i<pivotInputs.length; i++) {
+        if (elem.id != pivotInputs[i] && elem.value != '' && elem.value == $(pivotInputs[i]).value) {
+            select_list_selected_index = elem.selectedIndex;
+            text = elem.options[select_list_selected_index].text
+            alert("Please select a different field other than '"+text+"'");
+            elem.value='';
+        }
+    }
+}
+function validatePivotForm() {
+    if ($('fld_colfld1').value == '' || $('fld_rowfld1').value == '' || $('fld_datafld1').value == '') {
+        alert("Please select a valid column field, row fields and data field for pivot table.");
+        return false;
+    }
+    return true;
+}
