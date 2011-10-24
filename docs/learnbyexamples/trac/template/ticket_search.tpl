e37250de9aefe7df34db7de9583b6b39ac785bda
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-10-24 07:40:28 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@459 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ docs/learnbyexamples/trac/template/ticket_search.tpl
@@ -0,0 +1,79 @@
+<form id="{$form.name}" name="{$form.name}">
+<div style="padding-left:25px; ">
+
+	<div style="padding-left:0px;">
+		<h2><a id="{$form.name}_content_toggler" href="#" class="shrink" onclick="toggleDisplay('{$form.name}_content');"></a>Search Ticket</h2>
+		<div id="{$form.name}_content" style="display:block;padding-left:10px;">
+		<div class="" style="height:160px;">			
+			{foreach name=phone item=item key=itemName from=$dataPanel}
+				{if $item.elementset eq "General"}
+					<table  id="{$itemName}_container" class="input_row" style="float:left;">							
+					<tr>
+						<td >	
+							<label style="text-align:left;width:80px; ">{$item.label}</label>
+						</td>
+						<td>				
+							<span class="label_text" style="width:225px;">{$item.element}</span>					
+						</td>
+					</tr>
+					</table>
+				{/if}						
+			{/foreach}			
+		</div>
+		
+		<div style="display:block; padding:5px 5px 0px 0px; width:650px;">
+			{foreach item=item key=itemName from=$dataPanel}
+				{if $item.elementset eq "Content"}		
+					<table  id="{$itemName}_container" class="input_row">								
+					<tr>
+						<td >	
+							<label style="text-align:left; width:80px; ">{$item.label}</label>
+						</td>
+						<td>				
+							<span class="label_text" style="width:550px;">{$item.element}</span>					
+						</td>
+					</tr>
+					</table>	
+				{/if}						
+			{/foreach}		
+		</div>
+		
+		<div style="height:25px;">
+			{$dataPanel.fld_owner_id.element}
+			{foreach name=phone item=item key=itemName from=$dataPanel}
+				{if $item.elementset eq "Contact" and $item.type != "Hidden"}
+					<table  id="{$itemName}_container" class="input_row" style="float:left;">						
+					<tr>
+						<td>	
+							<label style="text-align:left;width:80px; ">{$item.label}</label>
+						</td>
+						<td>				
+							<span class="label_text" style="width:225px;">{$item.element}</span>					
+						</td>
+					</tr>
+					</table>
+				{/if}						
+			{/foreach}			
+		</div>
+
+  		<div style="clear:both"></div>
+  
+		<div style="height:10px;"></div>
+		<p class="input_row">
+			
+			{foreach item=elem from=$actionPanel}
+				{$elem.element}
+			{/foreach}
+		</p>
+		</div>
+	</div>
+	
+	<div style="height:10px;">
+	<div id='{$form.name}.load_disp' style="display:none;">
+	<img  src="{$image_url}/form_ajax_loader.gif"/>
+	</div>
+	</div>
+	
+</div>
+<hr style="width:700px; color:white; maring-left: 20px;margin-bottom:10px; "></hr>
+</form>
---
+++ docs/learnbyexamples/trac/ticket/form/TicketSearchForm.php
@@ -0,0 +1,134 @@
+<?php
+include_once (MODULE_PATH.'/trac/email/TicketEmailService.php');
+
+    class TicketSearchForm extends EasyForm 
+    { 
+    protected $ticketListForm = "trac.ticket.form.TicketResultsForm";
+    protected $ticketQueryDO = "trac.ticket.do.TicketQueryDO";
+
+    public function searchTicket()
+    {
+        include_once(OPENBIZ_BIN."/easy/SearchHelper.php");
+        $searchRule = "";
+        foreach ($this->m_DataPanel as $element)
+        {
+            if (!$element->m_FieldName)
+                continue;
+
+            $value = BizSystem::clientProxy()->getFormInputs($element->m_Name);
+            if($element->m_FuzzySearch=="Y")
+            {
+                $value="*$value*";
+            }
+            if ($value)
+            {
+                $searchStr = inputValToRule($element->m_FieldName, $value, $this);
+                if ($searchRule == "")
+                    $searchRule .= $searchStr;
+                else
+                    $searchRule .= " AND " . $searchStr;
+            }
+        }
+        
+        $searchRuleBindValues = QueryStringParam::getBindValues();
+        
+        // get the ticket search results form object, set the search rule and redraw that form
+        $listFormObj = BizSystem::getObject($this->ticketListForm);
+        $listFormObj->setSearchRule($searchRule, $searchRuleBindValues);
+        $listFormObj->rerender();
+    }
+
+    public function saveSearch()
+    {
+        // get non-empty input field value pairs
+        $recArr = $this->readInputRecord();
+        foreach ($recArr as $k=>$v) {	// ignore the empty inputs
+            if (empty($v))
+                unset($recArr[$k]);
+        }
+        $saveAs = BizSystem::clientProxy()->getFormInputs("input_saveas");
+        
+        // serialize it
+        $data = serialize($recArr);
+        
+        // save them in the table
+        $queryObj = BizSystem::getObject($this->ticketQueryDO);
+        $records = $queryObj->directFetch("[name]='$saveAs'",1);
+        if (count($records)>0) {
+            $oldRec = $records[0];
+            $dataRec = new DataRecord($oldRec, $queryObj);
+        }
+        else {
+            $dataRec = new DataRecord(null, $queryObj);
+        }
+        $dataRec['name'] = $saveAs;
+        $dataRec['search_rules'] = $data;
+
+        try
+        {
+            $dataRec->save();
+        }
+        catch (BDOException $e)
+        {
+            $this->processBDOException($e);
+            return;
+        }
+        
+        // get the message string from a key QUERY_IS_SAVED which is defined in message file trac/message/trac.msg
+        $message = $this->getMessage("QUERY_IS_SAVED", array($saveAs));
+        // display an alert dialog on browser with given message
+        BizSystem::clientProxy()->showClientAlert($message);
+        return;
+    }
+   	
+    public function fetchDataSet()
+    {
+        $this->prepareQuery();
+        
+        return parent::fetchDataSet();
+    }
+
+    protected function prepareQuery()
+    {
+        // handle search with saved query id
+        if (isset($_GET['qid']))
+        {
+            // fetch the saved query record
+            $queryObj = BizSystem::getObject($this->ticketQueryDO);
+            $record = $queryObj->fetchById($_GET['qid']);
+            
+            $queryData = unserialize($record['search_rules']);
+            $this->m_Title .= " [".$record['name']."]";
+            
+            include_once(OPENBIZ_BIN."/easy/SearchHelper.php");
+            $searchRule = "";
+            foreach ($queryData as $fieldName=>$value)
+            {
+                if ($value)
+                {
+                    $searchStr = inputValToRule($fieldName, $value, $this);
+                    if ($searchRule == "")
+                        $searchRule .= $searchStr;
+                    else
+                        $searchRule .= " AND " . $searchStr;
+                }
+            }
+            
+            $this->setFixSearchRule($searchRule,false);
+            $this->m_SearchRuleBindValues = QueryStringParam::getBindValues();
+        }
+        // handle search for my ticket or ticket reported by me
+        else if (isset($_GET['q'])) 
+        {
+            $profile = BizSystem::getUserProfile();
+            if ($profile) {
+                $userid = $profile['Id'];
+                if ($_GET['q'] == 'my')
+                    $this->setFixSearchRule("[owner_id]=$userid");
+                else if ($_GET['q'] == 'me')
+                    $this->setFixSearchRule("[reporter_id]=$userid");
+            }
+        }
+    }
+}
+?>
