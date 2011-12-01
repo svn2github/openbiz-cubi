615450c12b748ef407ea26d15cb4180a0e39866f
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-01 10:08:36 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@583 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/myaccount/mod.install.sql
+++ cubi/modules/myaccount/mod.install.sql
@@ -17,6 +17,13 @@ CREATE TABLE IF NOT EXISTS `preference` (
   KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+INSERT INTO `preference` (`id`, `user_id`, `section`, `name`, `value`, `type`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES
+(1, 0, 'General', 'group_data_share', '1', 'Radio', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(2, 0, 'Data Sharing', 'owner_perm', '3', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(3, 0, 'Data Sharing', 'group_perm', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(4, 0, 'Data Sharing', 'other_perm', '0', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43');
+
+
 /*schema for the table `user_widget` */
 
 DROP TABLE IF EXISTS `user_widget`;
