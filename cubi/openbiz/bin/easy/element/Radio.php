ce421387beeb47b26edad825ba8249c9bc971a09
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-01 10:32:17 +0000

    fix a bug of getting 0 value
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@586 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/bin/easy/element/Radio.php
+++ cubi/openbiz/bin/easy/element/Radio.php
@@ -40,7 +40,7 @@ class Radio extends OptionElement
         $style = $this->getStyle();
         $func = $this->getFunction();
 
-        $value = $this->getValue() ? $this->getValue() : $this->getDefaultValue();
+        $value = $this->getValue()!='null' ? $this->getValue() : $this->getDefaultValue();
         foreach ($fromList as $option) {        	
             $checkedStr = ($option['val'] == $value) ? "CHECKED" : "";
             $sHTML .= "<label style=\"text-align:left;\"><INPUT TYPE=RADIO NAME='".$this->m_Name."' ID=\"" . $this->m_Name ."_".$option['val']."\" VALUE=\"" . $option['val'] . "\" $checkedStr $disabledStr $this->m_HTMLAttr $func />" . $option['txt'] . "</label>";
