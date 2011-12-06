c7af79b4acbb8b6b3bf8ec8a0290cdd37718802e
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-06 05:19:39 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@655 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ apps/collab/msgbox/form/MessageComposeForm.php
@@ -0,0 +1,73 @@
+<?php 
+class MessageComposeForm extends EasyForm
+{
+	public function fetchData()
+	{
+		if(!(int)$this->m_RecordId){
+			$dataRec = new DataRecord(null, $this->getDataObj());
+	        $dataRec["subject"] = "no subject";
+			$dataRec["send_status"] = "draft";
+	        $recId = $dataRec->save();
+			$this->m_RecordId = $recId;
+			$this->m_ActiveRecord = $dataRec;
+			$this->setActiveRecord($dataRec->toArray());
+		}
+ 		$result = parent::fetchData();
+		return $result;
+	}
+	
+	public function SendMessage()
+	{
+		$currentRec = parent::fetchData();
+        $recArr = $this->readInputRecord();
+        
+        try
+        {
+            $this->ValidateForm();
+        }
+        catch (ValidationException $e)
+        {
+            $this->processFormObjError($e->m_Errors);
+            return;
+        }
+        $recArr['send_status'] = 'sent';
+        if ($this->_doUpdate($recArr, $currentRec) == false)
+            return;        
+
+        if ($this->m_ParentFormName)
+        {
+            $this->close();
+            $this->renderParent();
+        }
+
+        $this->processPostAction();
+	}
+	
+	public function SaveMessage(){
+		$currentRec = $this->getDataObj()->fetchById($this->m_RecordId);			
+        $recArr = $this->readInputRecord();
+
+        try
+        {
+            $this->ValidateForm();
+        }
+        catch (ValidationException $e)
+        {
+            $this->processFormObjError($e->m_Errors);
+            return;
+        }
+        $recArr['send_status'] = 'draft';
+        if ($this->_doUpdate($recArr, $currentRec) == false)
+            return;
+        $this->m_SearchRule = "";
+            
+        // in case of popup form, close it, then rerender the parent form
+        if ($this->m_ParentFormName)
+        {
+            $this->close();
+            $this->renderParent();
+        }
+        $this->processPostAction();
+	}
+}
+?>
