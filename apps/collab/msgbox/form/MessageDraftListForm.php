b81f3cddbd2bb69565614de29ce8b49fac59d6ef
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-08 03:02:58 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@673 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/msgbox/form/MessageDraftListForm.php
+++ apps/collab/msgbox/form/MessageDraftListForm.php
@@ -1,6 +1,19 @@
 <?php
 class MessageDraftListForm extends EasyForm
 {
-	
+	public function fetchDataSet()
+	{
+		$resultSet = parent::fetchDataSet();
+		$recordSet = array();
+		foreach ($resultSet as $record)
+		{
+			if($record['subject']=="")
+			{
+				$record['subject']="[no subject]";
+			}
+			array_push($recordSet,$record);
+		}
+		return $recordSet;
+	}
 }
 ?>
