98da2e1f9b1dc87e78918c9633c332ebad6b97f2
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-10 06:57:28 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@704 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/msgbox/form/MessageComposeForm.php
+++ apps/collab/msgbox/form/MessageComposeForm.php
@@ -9,30 +9,36 @@ class MessageComposeForm extends EasyForm
 	}
 	
 	public function fetchData()
-	{
-		if($_GET["F"]!='RPCInvoke'){		
-				
+	{						
+		if($_GET["F"]!='RPCInvoke')
+		{		
 			$dataRec = new DataRecord(null, $this->getDataObj());
 	        $dataRec["subject"] = "";
 			$dataRec["send_status"] = "draft";
 	        $recId = $dataRec->save();
 			$this->m_RecordId = $recId;
-			$this->m_ActiveRecord = $dataRec;
-			$this->setActiveRecord($dataRec->toArray());
-		}else{					
+			$this->m_ActiveRecord = $dataRec;			
+			$this->setActiveRecord($dataRec->toArray());							
+		}
+		else
+		{					
 			if($this->m_RecordId)
 			{
-				$this->m_SearchRule = "[Id]='$this->m_RecordId'";															
+				$this->m_SearchRule = "[Id]='$this->m_RecordId'";	
+				$this->m_ActiveRecord = null;														
 			}
-			
-		}					
+			else
+			{
+				$this->m_SearchRule = "";
+			}	
+		}
  		$result = parent::fetchData();
  		if($result['send_status']=='draft'){
  			$result["type_color"] = 'afe8fb'; 				
  		}else{
  			$result["type_color"] = 'c8c8c8';
  		}
- 		$result["send_status"] = ucwords($result["send_status"]);
+ 		$result["send_status_display"] = ucwords($result["send_status"]);
  		$result["recipient_to"] = $this->getRecipientList("Recipient");
 		$result["recipient_cc"] = $this->getRecipientList("CC");
 		$result["recipient_bcc"] = $this->getRecipientList("BCC");
@@ -62,6 +68,7 @@ class MessageComposeForm extends EasyForm
             return;
         }
         $recArr['send_status'] = 'draft';
+
         if ($this->_doUpdate($recArr, $currentRec) == false)
             return;        
 		parent::loadDialog($formName,$id);	
