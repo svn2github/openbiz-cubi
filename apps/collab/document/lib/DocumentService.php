09b1166a8c1c0899b89cfd522df8677de0fdf3bd
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-14 04:14:36 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@759 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/document/lib/DocumentService.php
+++ apps/collab/document/lib/DocumentService.php
@@ -1,9 +1,33 @@
 <?php
 class DocumentService
 {
+	protected  $m_DocumentReversionDO = 'collab.document.do.DocumentReversionDO';
+	
 	public function saveReversion($data)
 	{
-		
+		$data = $data->getActiveRecord();
+		$reversionRec = array(
+			"document_id"	=>	$data['Id'],
+			"reversion"		=>	$this->getLastVersion($data['Id'])+1,
+			"title"			=>	$data['title'],
+			"description"	=>	$data['description'],
+			"content"		=>	$data['content']
+		);
+		BizSystem::getObject($this->m_DocumentReversionDO)->insertRecord($reversionRec);
+	}
+	
+	public function getLastVersion($document_id)
+	{
+		$lastVersions = BizSystem::getObject($this->m_DocumentReversionDO)->directFetch("[document_id]='$document_id'",-1,0,"[reversion] DESC");
+		if($lastVersions)
+		{
+			$lastVersion = $lastVersions[0];
+			return $lastVersion['document_id'];			
+		}
+		else
+		{
+			return 0;
+		}		
 	}
 }
 ?>
