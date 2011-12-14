c2462481c05b0641844950198f8bfbbc19946196
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-14 04:55:06 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@763 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/document/lib/DocumentService.php
+++ apps/collab/document/lib/DocumentService.php
@@ -6,14 +6,31 @@ class DocumentService
 	public function saveReversion($data)
 	{
 		$data = $data->getActiveRecord();
-		$reversionRec = array(
-			"document_id"	=>	$data['Id'],
-			"reversion"		=>	$this->getLastVersion($data['Id'])+1,
-			"title"			=>	$data['title'],
-			"description"	=>	$data['description'],
-			"content"		=>	$data['content']
-		);
-		BizSystem::getObject($this->m_DocumentReversionDO)->insertRecord($reversionRec);
+		$lastData = $this->getLastVersion($data['Id']);
+		if($this->isDiff($data,$lastData))
+		{
+			$reversionRec = array(
+				"document_id"	=>	$data['Id'],
+				"reversion"		=>	(int)$lastData['reversion']+1,
+				"title"			=>	$data['title'],
+				"description"	=>	$data['description'],
+				"content"		=>	$data['content']
+			);
+			BizSystem::getObject($this->m_DocumentReversionDO)->insertRecord($reversionRec);
+		}
+	}
+	
+	public function isDiff($data1,$data2)
+	{
+		if($data1['title']==$data2['title'] &&
+			$data1['description']==$data2['description'] &&
+			$data1['content']==$data2['content'] 
+			)
+			{
+				return false;
+			}else{
+				return true;
+			}
 	}
 	
 	public function getLastVersion($document_id)
@@ -22,11 +39,11 @@ class DocumentService
 		if($lastVersions)
 		{
 			$lastVersion = $lastVersions[0];
-			return $lastVersion['document_id'];			
+			return $lastVersion;			
 		}
 		else
 		{
-			return 0;
+			return null;
 		}		
 	}
 }
