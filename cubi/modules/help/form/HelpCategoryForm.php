0b6356420fd9365bff38dce7bbd178bf4c5bcd99
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-26 08:57:24 +0000

    add a validate logic on edit a category record , cannot move a item to itself
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@864 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/help/form/HelpCategoryForm.php
+++ cubi/modules/help/form/HelpCategoryForm.php
@@ -11,6 +11,54 @@ class HelpCategoryForm extends EasyFormTree
 		return $result;
 	}
 	
- 
+	protected function validateForm($cleanError = true)
+    {
+    	
+    	$result = parent::validateForm($cleanError);
+    	
+        if($result)
+        {
+        	$parentId = $this->m_DataPanel->get("fld_parent_id")->m_Value;
+        	$currentId = $this->m_DataPanel->get("fld_Id")->m_Value;
+		    if ($parentId == $currentId && strtoupper($this->m_FormType)=='EDIT')
+	        {
+	            $errorMessage = $this->getMessage("FORM_PARENT_SHOULD_NOT_SAME_AS_ITSELF");                                
+	            $this->m_ValidateErrors[$element->m_Name] = $errorMessage;
+	        }           
+			
+	        if (!$this->hasRootExcept($currentId) && strtoupper($this->m_FormType)=='EDIT')
+	        {
+	        	$errorMessage = $this->getMessage("FORM_ITS_LAST_ROOT_CATEGORY");                                
+	            $this->m_ValidateErrors[$element->m_Name] = $errorMessage;
+	        }
+	        
+	        /* todo:
+        	if ($this->isMovedToChild($parentId,$currentId) && strtoupper($this->m_FormType)=='EDIT')
+	        {
+	        	$errorMessage = $this->getMessage("FORM_CANNOT_MOVE_ITS_CHILD");                                
+	            $this->m_ValidateErrors[$element->m_Name] = $errorMessage;
+	        }
+	        */
+	        
+	        if (count($this->m_ValidateErrors) > 0)
+	        {
+	            throw new ValidationException($this->m_ValidateErrors);
+	            return false;
+	        }
+        }
+        return $result;
+    } 
+    
+    protected function hasRootExcept($currentId)
+    {
+    	$rs = $this->getDataObj()->fetchOne("[PId]=0 AND [Id]!='".(int)$currentId."'");
+    	if($rs){
+	    	return true;
+    	}else{
+    		return false;
+    	}    
+    }
+    
+
 }
 ?>
