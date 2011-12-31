9e7eca4c554e869ddb4de423c8b00866d3093a7a
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2011-12-31 04:51:35 +0000

    Change the progress update from updateform to updatefields. This is to avoid rendering form on each ajax call.
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@944 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/js/Openbiz.PackageForm.js
+++ cubi/js/Openbiz.PackageForm.js
@@ -13,7 +13,7 @@ Openbiz.PackageForm = Class.create(Openbiz.Form,
     {
         fieldValues.each(function(tgt_ctnt) {
             // special handling progress_bar update
-            if (tgt_ctnt.target == 'progress_bar') {
+            if (tgt_ctnt.target == 'fld_status') {
                 inst_info = tgt_ctnt.content.split('|', 4);
                 state = inst_info[0];                 
                 log = inst_info[1]; 
@@ -24,8 +24,20 @@ Openbiz.PackageForm = Class.create(Openbiz.Form,
                 	) {
                     setTimeout(function() {
                         Openbiz.CallFunction("package.local.form.PackageInstallerForm.getProgress()",null);
-                    }, 1000);
+                    }, 2000);
                 }
+            }
+            else if (tgt_ctnt.target == 'fld_total_progress' || tgt_ctnt.target == 'fld_download_progress') {
+                // use jquery selector to set the progress
+                var barobj = $j('#'+tgt_ctnt.target).find('.bar_bg')[0];
+                var bardata = $j(barobj).find('.bar_data')[0];
+                var bardatabg = $j(barobj).find('.bar_data_bg')[0];
+                var barval = $j('#'+tgt_ctnt.target).find('.value')[0];
+                barval.innerHTML = tgt_ctnt.content + "%";
+                var barobj_width = $j(barobj).css("width");
+                var progress_width = Math.round(parseInt(tgt_ctnt.content)/100*parseInt(barobj_width));
+                $j(bardata).css("width", progress_width+'px');
+                $j(bardatabg).css("width", progress_width+'px');
             }
             else {
                 $(tgt_ctnt.target).value = tgt_ctnt.content;
