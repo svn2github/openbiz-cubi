da3411ccdef904eacc488cfaa048a4fa04e2f808
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-01-14 18:05:07 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1308 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/location/element/LocationPreview.php
+++ cubi/modules/location/element/LocationPreview.php
@@ -10,7 +10,7 @@ class LocationPreview extends Element
 		$imgsrc="http://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longtitude&zoom=10&size=380x240&maptype=roadmap
 &markers=color:red%7Clabel:$title%7C$latitude,$longtitude&sensor=false
 		";
-		$HTML = "<a target=\"_blank\" href=\"$imgsrc\"><img src=\"$imgsrc\" border=\"0\" /></a>";
+		$HTML = "<a target=\"_blank\" href=\"$imgsrc\"><img width=\"380\" height=\"240\" src=\"$imgsrc\" border=\"0\" /></a>";
 		return $HTML;
 	}
 }
