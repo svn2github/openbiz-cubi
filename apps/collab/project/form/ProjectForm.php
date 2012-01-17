80eaeaee8249ce8f568dc3018d8ae8e43711d5e1
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-01-17 11:48:48 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1401 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/project/form/ProjectForm.php
+++ apps/collab/project/form/ProjectForm.php
@@ -1,6 +1,14 @@
 <?php 
 class ProjectForm extends EasyForm
 {
-	
+	public function UpdateProjectStatus($id, $fld_name, $value)
+	{
+		if($value == 1){
+    		$value_xor = 2;  		
+    	}else{
+    		$value_xor = 1;
+    	}    	
+		return $this->updateFieldValue($id,$fld_name,$value_xor);		
+	}
 }
 ?>
