c40dd63da7a844c80b9bd1e7e86b6c3d78eb3d66
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-01-18 05:22:07 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1410 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/task/widget/TaskMultiPickForm.php
+++ apps/collab/task/widget/TaskMultiPickForm.php
@@ -1,6 +1,13 @@
 <?php 
 class TaskMultiPickForm extends PickerForm
 {
-	
+	public function fetchDataSet()
+	{
+		$svcObj = BizSystem::GetService(DATAPERM_SERVICE);
+	    $dataPermSQLRule = $svcObj->buildSqlRule('update',true);
+	    $this->m_FixSearchRule = $dataPermSQLRule;
+		$resultSet = parent::fetchDataSet();		
+		return $resultSet;
+	}	
 }
 ?>
