18d36ff7fa3e7f65c32efbbebc4fb3270d4be48a
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-01-20 08:58:08 +0000

    use help as the sample of custom module load handler.
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1463 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/help/lib/HelpLoadHandler.php
@@ -0,0 +1,17 @@
+<?php
+include_once (MODULE_PATH."/system/lib/ModuleLoadHandler.php");
+
+class HelpLoadHandler implements ModuleLoadHandler
+{
+    public function beforeLoadingModule($moduelLoader)
+    {
+        echo "--- Test HelpLoadHandler::beforeLoadingModule \n";
+    }
+    
+    public function postLoadingModule($moduelLoader)
+    {
+        echo "--- Test HelpLoadHandler::postLoadingModule \n";
+    }
+}
+
+?>
