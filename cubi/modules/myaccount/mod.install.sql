ed36f7b39da36167515751f5a22533e93cae1bf4
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-01-20 10:55:53 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1475 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/myaccount/mod.install.sql
+++ cubi/modules/myaccount/mod.install.sql
@@ -21,8 +21,20 @@ INSERT INTO `preference` (`id`, `user_id`, `section`, `name`, `value`, `type`, `
 (1, 0, 'General', 'group_data_share', '1', 'Radio', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
 (2, 0, 'Data Sharing', 'owner_perm', '3', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
 (3, 0, 'Data Sharing', 'group_perm', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
-(4, 0, 'Data Sharing', 'other_perm', '0', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43');
-
+(4, 0, 'Data Sharing', 'other_perm', '0', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(5, 0, 'General', 'siteurl', 'http://dev.openbiz.cn/cubi', 'InputText', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(6, 0, 'General', 'language', 'en_US', 'myaccount.form.LanguageSelector', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(7, 0, 'General', 'theme', 'default', 'myaccount.form.ThemeSelector', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(8, 0, 'General', 'appbuilder', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(9, 0, 'Timezone', 'continent', 'Asia', 'myaccount.form.ContinentSelector', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(10, 0, 'Timezone', 'timezone', 'Asia/Chongqing', 'myaccount.form.TimezoneSelector', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(11, 0, 'Login', 'smartcard_auth', '0', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(12, 0, 'Login', 'anti_spam', '0', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(13, 0, 'Login', 'language_selector', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(14, 0, 'Login', 'theme_selector', '0', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(15, 0, 'Login', 'keep_cookies', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(16, 0, 'Register', 'open_register', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43'),
+(17, 0, 'Register', 'find_password', '1', 'DropDownList', 1, '2011-12-01 01:57:43', 1, '2011-12-01 01:57:43');
 
 /*schema for the table `user_widget` */
 
