dfa7be30b1c7a09f5b32abc86c6e31b177e5d611
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-01-20 09:18:59 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1466 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/help/lib/HelpLoadHandler.php
+++ cubi/modules/help/lib/HelpLoadHandler.php
@@ -5,12 +5,12 @@ class HelpLoadHandler implements ModuleLoadHandler
 {
     public function beforeLoadingModule($moduelLoader)
     {
-        echo "--- Test HelpLoadHandler::beforeLoadingModule \n";
+        //echo "--- Test HelpLoadHandler::beforeLoadingModule \n";
     }
     
     public function postLoadingModule($moduelLoader)
     {
-        echo "--- Test HelpLoadHandler::postLoadingModule \n";
+        //echo "--- Test HelpLoadHandler::postLoadingModule \n";
     }
 }
 
