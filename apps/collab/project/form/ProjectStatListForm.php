92d91907e9a98702635aef66ff1604f5962905c7
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-03 17:14:40 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1695 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/project/form/ProjectStatListForm.php
+++ apps/collab/project/form/ProjectStatListForm.php
@@ -10,10 +10,11 @@ class ProjectStatListForm extends StatisticsListForm
 		$pri  =	abs(2-($parentForm->m_RecordId));
 		$searchRule = $parentForm->m_SearchRule;
 		if($cond!==null && $pri!==null){
-			$this->m_SearchRule="[condition]='$cond' AND [priority]='$pri'";
+			$this->m_SearchRule="AND ([condition]='$cond' AND [priority]='$pri')";
 		}
 
 		if($searchRule){
+			$searchRule = str_replace("AND [chart_type]  = :_v1", "", $searchRule);
 			$searchRule = str_replace("[chart_type]  = :_v1", "", $searchRule);		
 			$this->m_SearchRule = $searchRule.$this->m_SearchRule;
 		}
