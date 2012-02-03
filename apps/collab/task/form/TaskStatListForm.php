94ba2549c850ac7b4c61165f0afa43914f97294b
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-03 03:26:33 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1677 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ apps/collab/task/form/TaskStatListForm.php
@@ -0,0 +1,25 @@
+<?php 
+require_once MODULE_PATH.'/collab/statistics/form/StatisticsListForm.php';
+class TaskStatListForm extends StatisticsListForm
+{
+	public function fetchDataSet()
+	{
+		
+		$parentForm = BizSystem::getObject("collab.task.form.TaskStatReportForm");			
+		$sev = abs(4-$parentForm->m_CategoryId);
+		$pri  =	abs(2-($parentForm->m_RecordId));
+		$searchRule = $parentForm->m_SearchRule;
+		if($sev!==null && $pri!==null){
+			$this->m_SearchRule="[severity]='$sev' AND [priority]='$pri'";
+		}
+
+		if($searchRule){
+			$searchRule = str_replace("[chart_type]  = :_v1", "", $searchRule);		
+			$this->m_SearchRule = $searchRule.$this->m_SearchRule;
+		}
+		
+		$result = parent::fetchDataSet();		
+		return $result;
+	}
+}
+?>
