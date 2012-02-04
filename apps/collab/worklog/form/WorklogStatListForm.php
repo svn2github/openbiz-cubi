8ef59e3af3f02cc042b820348c7cfdf7d2a5b1d5
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-04 01:41:39 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1700 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/worklog/form/WorklogStatListForm.php
+++ apps/collab/worklog/form/WorklogStatListForm.php
@@ -8,7 +8,7 @@ class WorklogStatListForm extends EasyFormGrouping
 		$day = sprintf('%02d',$parentForm->m_CategoryId+1);
 		$workdate = $parentForm->m_Year.'-'.$parentForm->m_Month.'-'.$day;
 		//$searchRule = $parentForm->m_SearchRule;
-		if($parentForm->m_CategoryId){
+		if($day){
 			$this->m_SearchRule="[work_date]='$workdate'";
 		}
 		$result = parent::fetchDataGroup();		
