8696c8689ed0b9f2a556ec7f7c0a9154637390fe
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-06 06:23:30 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1758 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/contact/mod.install.sql
+++ cubi/modules/contact/mod.install.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `type_id` int(11) NOT NULL,
   `sortorder` int(11) NOT NULL,
   `user_id` int(11) DEFAULT '0',
-  `published` int(11) NOT NULL DEFAULT '0',
+  `published` int(11) NOT NULL DEFAULT '1',
   `default` int(11) DEFAULT '0',
   `access` varchar(255) DEFAULT NULL,
   `params` text,
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `contact_type` (
 
 
 INSERT INTO `contact_type` (`id`, `name`, `description`, `color`, `sortorder`, `published`, `group_id`, `group_perm`, `other_perm`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES
-(1, 'Colleague', 'Our company colleague''s contacts', '66c2ff', 45, 1, 1, 1, 1, 1, '2010-05-23 01:09:06', 1, '2012-02-05 10:59:07'),
+(1, 'Colleague', 'Our company colleagues contacts', '66c2ff', 45, 1, 1, 1, 1, 1, '2010-05-23 01:09:06', 1, '2012-02-05 10:59:07'),
 (2, 'Potential Client', 'Potential clients, People interested buy not start using our product yet.', 'b8ff7d', 45, 1, 1, 1, 1, 1, '2010-05-23 01:23:04', 1, '2012-02-05 11:02:01'),
 (3, 'Provider', 'All kind of providers. Like product hardware provider, printing service providers etc.', '7fff7f', 50, 1, 1, 1, 1, 1, '2010-05-23 01:34:12', 1, '2012-02-05 11:03:38'),
 (4, 'Existing Client', 'Client who already started using our products.', 'ffd042', 45, 1, 1, 1, 1, 1, '2010-05-23 01:34:39', 1, '2012-02-05 11:02:29'),
