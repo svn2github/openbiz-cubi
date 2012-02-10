74505318381b9f0eedebcfe13e29c3856ebdbb27
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-10 04:01:23 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1819 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/task/form/TaskTimesheetForm.php
+++ apps/collab/task/form/TaskTimesheetForm.php
@@ -154,10 +154,10 @@ class TaskTimesheetForm extends ChangeLogForm
 		);
 		
 		if($total_tasks){
-			$dataArr['not_started_wid'] = (int)($statData[0]/$total_tasks*$bar_width)-2;
-			$dataArr['in_progress_wid'] = (int)($statData[1]/$total_tasks*$bar_width)-2;
-			$dataArr['completed_wid'] = (int)($statData[2]/$total_tasks*$bar_width)-2;
-			$dataArr['other_wid'] = (int)(($statData[3]+(int)$statData[4])/$total_tasks*$bar_width)-2;
+			$dataArr['not_started_wid'] = (int)($statData[0]/$total_tasks*$bar_width);
+			$dataArr['in_progress_wid'] = (int)($statData[1]/$total_tasks*$bar_width);
+			$dataArr['completed_wid'] = (int)($statData[2]/$total_tasks*$bar_width);
+			$dataArr['other_wid'] = (int)(($statData[3]+(int)$statData[4])/$total_tasks*$bar_width);
 		}else{
 			$dataArr['not_started_wid'] = 2;
 			$dataArr['in_progress_wid'] = 2;
