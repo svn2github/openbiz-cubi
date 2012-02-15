480608b21945601548b760f39c9d78f596ba52a6
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-15 08:49:22 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@1960 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ apps/collab/document/form/DocumentForm.php
@@ -0,0 +1,17 @@
+<?php 
+class DocumentForm extends EasyForm
+{
+	public function SaveDoc()
+	{
+						
+		parent::updateRecord();
+		$message=$this->getMessage("DOC_SAVED");
+		$content="<div id=\"notice_save_msg\" class=\"noticeBox\" >$message</div>";	
+		$script="
+		<script>try{setTimeout(\"$('notice_save_msg').fade( {from: 1, to: 0});\",3000);}catch(e){};</script>
+		";
+		BizSystem::clientProxy()->updateClientElement('notice_save', $content);
+		BizSystem::clientProxy()->runClientScript($script);
+	}	
+}
+?>
