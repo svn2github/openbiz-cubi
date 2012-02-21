66aa21d0d4569564cf42620a05054fc6a8d26098
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-21 02:10:50 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2015 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/project/widget/ProjectSinglePickForm.php
+++ apps/collab/project/widget/ProjectSinglePickForm.php
@@ -3,9 +3,11 @@ class ProjectSinglePickForm extends PickerForm
 {
 	public function fetchDataSet()
 	{
+		/*
 		$svcObj = BizSystem::GetService(DATAPERM_SERVICE);
 	    $dataPermSQLRule = $svcObj->buildSqlRule('update',true);
 	    $this->m_FixSearchRule = $dataPermSQLRule;
+	    */
 		$resultSet = parent::fetchDataSet();		
 		return $resultSet;
 	}
