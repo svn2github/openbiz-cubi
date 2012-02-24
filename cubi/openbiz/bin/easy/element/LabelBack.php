30f1ef9ae0584766689ba10e1101858cd0dae8d1
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-24 12:00:52 +0000

    added lablelBack button
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2118 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/bin/easy/element/LabelBack.php
+++ cubi/openbiz/bin/easy/element/LabelBack.php
@@ -5,18 +5,7 @@ class LabelBack extends LabelText
 {
     protected function getLink()
     {
-        if ($this->m_Link == null)
-            return null;
-        $formobj = $this->getFormObj();
-        $defaultLink= Expression::evaluateExpression($this->m_Link, $formobj);
-        
-        $viewRefferedPage = $this->getFormObj()->getViewObject()->m_RefferPage;
-        
-        if(isset($viewRefferedPage)){
-        	return $viewRefferedPage;
-        }else{        
-        	return $defaultLink;
-        }
+        return "javascript:history.go(-1);";
     }
 
 }
