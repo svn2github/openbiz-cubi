2a7c1b429f3d60369875faec03b2d59642997cd5
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-02-28 14:14:00 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2177 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/bin/easy/element/LabelBar.php
+++ cubi/openbiz/bin/easy/element/LabelBar.php
@@ -37,13 +37,19 @@ class LabelBar extends ColumnBar {
         }
         $width_bar = (int)($width * $width_rate);
         
+    	if(!preg_match("/MSIE 6/si",$_SERVER['HTTP_USER_AGENT'])){
+			$bar_overlay="<span class=\"bar_data_bg\" style=\"".$bgcolor_str."height:".$height."px;width:".$width_bar."px;\"></span>";
+			$bar = "<span class=\"bar_data\" style=\"".$bgcolor_str."height:".$height."px;width:".$width_bar."px;\"></span>";
+		}else{
+			$bar = "<span class=\"bar_data\" style=\"".$bgcolor_str."height:".$height."px;width:".$width_bar."px;opacity: 0.4;filter: alpha(opacity=40);\"></span>";
+		}
+		
     	$sHTML = "
     	<span id=\"$id\" $func $style >
     		
-    		<span class=\"bar_bg\" style=\"height:".$height."px;width:".$width."px;\">
-    			
-    			<span class=\"bar_data\" style=\"".$bgcolor_str."height:".$height."px;width:".$width_bar."px;\"></span>	
-    			<span class=\"bar_data_bg\" style=\"".$bgcolor_str."height:".$height."px;width:".$width_bar."px;\"></span>	
+    		<span class=\"bar_bg\" style=\"height:".$height."px;width:".$width."px;\">    			
+    		$bar_overlay
+    		$bar	 
     		</span>
     		
     		<span class=\"value\" style=\"text-align:left;text-indent:10px;\">$value".$this->m_DisplayUnit."</span>
