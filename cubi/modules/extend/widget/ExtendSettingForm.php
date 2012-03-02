f32ab1bd6355b67a8d9aac6bf9783e195ffd3812
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-02 09:25:46 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2214 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/extend/widget/ExtendSettingForm.php
+++ cubi/modules/extend/widget/ExtendSettingForm.php
@@ -1,6 +1,19 @@
 <?php 
 class ExtendSettingForm extends EasyForm
 {
+	public $m_ParentFormElementMeta;
+	
+	public function getSessionVars($sessionContext)
+    {
+        $sessionContext->getObjVar($this->m_Name, "ParentFormElementMeta", $this->m_ParentFormElementMeta);
+        return parent::getSessionVars($sessionContext);
+    }
+
+    public function setSessionVars($sessionContext)
+    {
+        $sessionContext->setObjVar($this->m_Name, "ParentFormElementMeta", $this->m_ParentFormElementMeta);
+        return parent::setSessionVars($sessionContext);       
+    }	
 	
 }
 ?>
