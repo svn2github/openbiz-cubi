67a5053d845fda821e54384d651f0076220eb30d
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-03 07:27:27 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2261 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/extend/lib/ExtendLoadHandler.php
@@ -0,0 +1,28 @@
+<?php
+include_once (MODULE_PATH."/system/lib/ModuleLoadHandler.php");
+
+class ExtendLoadHandler implements ModuleLoadHandler
+{
+    public function beforeLoadingModule($moduelLoader)
+    {
+    }
+    
+    public function postLoadingModule($moduelLoader)
+    {
+    	
+    	$roleRec = BizSystem::getObject("system.do.RoleDO")->fetchOne("[name]='Cubi Member'");
+    	$roleId = $roleRec['Id'];
+    	
+    	$actionRec = BizSystem::getObject("system.do.AclActionDO")->fetchOne("[module]='extend' AND [resource]='extend' AND [action]='access'");
+    	$actionId = $actionRec["Id"];
+    	
+    	$aclRecord = array(
+    		"role_id" =>  $roleId,
+    		"action_id" => $actionId,
+    		"access_level" => 1
+    	);
+    	BizSystem::getObject("system.do.AclRoleActionDO")->insertRecord($aclRecord);
+    }
+}
+
+?>
