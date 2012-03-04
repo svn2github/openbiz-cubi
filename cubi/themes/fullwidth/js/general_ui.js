fe26027757d2e5c66f000d115c0877babda42c17
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-04 14:10:51 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2307 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/themes/fullwidth/js/general_ui.js
+++ cubi/themes/fullwidth/js/general_ui.js
@@ -166,13 +166,15 @@ function control_elementset(formName,id,action)
 {
 	switch(action){
 	case "hide":		
-		setTimeout("$('element_set_panel_"+id+"').style.display = 'none'",100);		
+		$('element_set_panel_'+id).style.position="absolute";
+		$('element_set_panel_'+id).style.left="-10000px";		
 		$('element_set_btn_'+id).className='expand';			
 		break;
 	case "show":
 		default:		
-		if($('element_set_panel_'+id).style.display=="none"){
-			$('element_set_panel_'+id).style.display = 'block';
+		if($('element_set_panel_'+id).style.position=="absolute"){
+			$('element_set_panel_'+id).style.position="static";
+			$('element_set_panel_'+id).style.left="";
 			$('element_set_panel_'+id).fade({duration: 0.5,from: 0.5, to: 1}); 
 			$('element_set_btn_'+id).className='shrink';
 		}
