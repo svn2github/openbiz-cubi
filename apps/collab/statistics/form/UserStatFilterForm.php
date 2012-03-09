dfff4aeea807c9dbd5808f28e54d1b2b3a124974
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-09 10:53:59 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2439 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ apps/collab/statistics/form/UserStatFilterForm.php
@@ -0,0 +1,39 @@
+<?php
+class UserStatFilterForm extends EasyForm
+{
+	public $m_Year;
+	public $m_Month;
+	public $m_RecordId;
+	
+	public function runSearch()
+	{	
+		$data = $this->readInputRecord();		
+		$this->m_Year = $data['year'];
+		$this->m_Month = $data['month'];
+		unset( $_POST['year_selector'] );
+		unset( $_POST['month_selector'] );
+		//$result = parent::runSearch();
+
+		$recId = $this->getDataObj()->fetchOne("[username]='".$data['username']."'")->Id;		
+		$this->selectRecord($recId);
+		
+		return $result;
+	}
+	
+	
+    public function getSessionVars($sessionContext)
+    {    	
+    	$sessionContext->getObjVar($this->m_Name, "Year", $this->m_Year);
+        $sessionContext->getObjVar($this->m_Name, "Month", $this->m_Month);
+        return parent::getSessionVars($sessionContext);
+    }
+
+
+    public function setSessionVars($sessionContext)
+    {
+    	$sessionContext->setObjVar($this->m_Name, "Year", $this->m_Year);
+        $sessionContext->setObjVar($this->m_Name, "Month", $this->m_Month);
+        return parent::setSessionVars($sessionContext);        
+    }	
+}
+?>
