21cc87798d1b9b311fb72f32b5afe0c73c8bef72
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-09 01:26:12 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2411 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/common/element/LabelTitle.php
@@ -0,0 +1,18 @@
+<?php 
+class LabelTitle extends LabelText
+{
+	public function getIDPrefix()
+	{
+		$rec = $this->getFormObj()->getActiveRecord();
+		$id = $rec["Id"];
+		$id_display = "<span class=\"title_id\" >$id</span>";
+		return $id_display;
+	}
+	
+	public function render(){
+		$sHTML = parent::render();
+		$sHTML = $sHTML.$this->getIDPrefix();
+		return $sHTML;
+	}
+}
+?>
