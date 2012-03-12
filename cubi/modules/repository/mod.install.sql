70c2d5b662bd68bdeaa677b08773719a38b30ed2
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-12 04:21:34 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2493 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/repository/mod.install.sql
@@ -0,0 +1,69 @@
+DROP TABLE IF EXISTS `repo_install_log`;
+CREATE TABLE IF NOT EXISTS `repo_install_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_id` int(11) NOT NULL,
+  `release_id` int(11) NOT NULL,
+  `remote_ip` varchar(255) NOT NULL,
+  `remote_siteurl` varchar(255) NOT NULL,
+  `remote_operator` varchar(255) NOT NULL,
+  `create_by` int(11) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+DROP TABLE IF EXISTS `repo_app`;
+CREATE TABLE IF NOT EXISTS `repo_app` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `package_id` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `type` varchar(64) NOT NULL,
+  `categroy_id` int(64) NOT NULL,
+  `author` varchar(128) NOT NULL,
+  `description` text,
+  `status` int(2) DEFAULT NULL,
+  `release_time` datetime DEFAULT NULL,
+  `create_by` int(11) DEFAULT '1',
+  `create_time` datetime DEFAULT NULL,
+  `update_by` int(11) DEFAULT '1',
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `package_id` (`package_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+
+DROP TABLE IF EXISTS `repo_category`;
+CREATE TABLE IF NOT EXISTS `repo_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `attrs` text,
+  `publish` int(11) DEFAULT NULL,
+  `sort_order` int(11) NOT NULL DEFAULT '10',
+  `create_time` datetime DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+
+DROP TABLE IF EXISTS `repo_release`;
+CREATE TABLE IF NOT EXISTS `repo_release` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_id` int(11) NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `pltfm_ver` varchar(64) NOT NULL,
+  `description` text,
+  `file` text,
+  `create_by` int(11) DEFAULT '1',
+  `create_time` datetime DEFAULT NULL,
+  `update_by` int(11) DEFAULT '1',
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `app_id` (`app_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
---
+++ cubi/modules/repository/mod.uninstall.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `repo_install_log`;
+DROP TABLE IF EXISTS `repo_app`;
+DROP TABLE IF EXISTS `repo_category`;
+DROP TABLE IF EXISTS `repo_release`;
