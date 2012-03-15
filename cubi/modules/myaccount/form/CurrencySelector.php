8fd1ee58fc37cb723514cbc2c08121271655a2c6
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-15 02:53:57 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2570 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/myaccount/form/CurrencySelector.php
+++ cubi/modules/myaccount/form/CurrencySelector.php
@@ -8,9 +8,12 @@ class CurrencySelector extends Listbox{
 		$locale = new Zend_Locale($current_locale);
 			
 		$current_currency = DEFAULT_CURRENCY;		
-		
+		if($current_currency){
+			$current_currency = "USD";
+		}
 		require_once('Zend/Currency.php');
-		$currency = new Zend_Currency(DEFAULT_LANGUAGE,$current_locale);
+		
+		$currency = new Zend_Currency($current_currency,$current_locale);
 		$currencyList = $currency->getCurrencyList();
     	foreach ($currencyList as $currency_code => $country){
     		
