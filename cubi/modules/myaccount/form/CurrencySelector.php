83a77e16a1beb1ac94b6ea9378f2126067eb7db9
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-15 02:22:47 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2564 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/myaccount/form/CurrencySelector.php
@@ -0,0 +1,28 @@
+<?php 
+require_once(OPENBIZ_BIN."easy/element/DropDownList.php");
+class CurrencySelector extends Listbox{
+    function getFromList(&$list){
+
+		$current_locale = I18n::getCurrentLangCode();		
+		require_once('Zend/Locale.php');
+		$locale = new Zend_Locale($current_locale);
+			
+		$current_currency = DEFAULT_CURRENCY;		
+		
+		require_once('Zend/Currency.php');
+		$currency = new Zend_Currency('CNY',$current_locale);
+		$currencyList = $currency->getCurrencyList();
+    	foreach ($currencyList as $currency_code => $country){
+    		
+    				$display_name = $currency->getName($currency_code,$current_locale);
+    				if($display_name){    					
+			    		array_push($list,array("val"=>$currency_code,
+			    								"txt"=>$display_name
+			    		));
+    				}
+    		
+    	}
+    	return $list;
+    }
+}
+?>
