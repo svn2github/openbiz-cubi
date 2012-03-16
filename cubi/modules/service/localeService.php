4b3a166b37510eab7818da1022bfa164a432000b
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-16 03:21:30 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2598 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/service/localeService.php
+++ cubi/modules/service/localeService.php
@@ -1,13 +1,19 @@
 <?php 
 class localeService 
 {
-	public function getDefaultLangName()
+	public function getDefaultLangName($lang)
 	{
 		$current_locale = I18n::getCurrentLangCode();		
 		require_once('Zend/Locale.php');
 		$locale = new Zend_Locale($current_locale);
-		$display_name = Zend_Locale::getTranslation(DEFAULT_LANGUAGE,'language',$current_locale);		
-		return $display_name;
+		$display_name = Zend_Locale::getTranslation($lang,'language',$locale);
+		if($display_name)
+		{
+			return $display_name;
+		}
+		else{		
+			return $lang;
+		}
 	}
 }
 ?>
