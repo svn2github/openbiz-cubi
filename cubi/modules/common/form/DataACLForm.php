92dac4d935b11e966105d549816a3d90f89d3c40
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-16 06:33:59 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2619 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/common/form/DataACLForm.php
+++ cubi/modules/common/form/DataACLForm.php
@@ -2,6 +2,8 @@
 require_once('DataSharingForm.php');
 class DataACLForm extends DataSharingForm
 {
+	public $m_AclDO = "common.do.DataACLDO";
+	
 	public function fetchDataSet()
 	{
 		$prtRecord = $this->fetchData();
@@ -9,5 +11,47 @@ class DataACLForm extends DataSharingForm
 		$result =  parent::fetchDataSet();
 		return $result;
 	}
+	
+	
+	public function addAcl()
+	{
+		$inputs = $this->readInputs();
+		$acl_user = $inputs['fld_acl_uid'];
+		$acl_perm = $inputs['fld_acl_perm'];
+		
+		//get UserID
+		$userRec = BizSystem::getObject("system.do.UserDO",1)->fetchOne("[username]='$acl_user'");
+		$acl_user_id = $userRec['Id'];
+		
+		$parent_record_id = $this->m_ParentRecordId;
+		
+		//get parent do table
+		$prtForm = $this->m_ParentFormName;
+		$prtFormObj = BizSystem::GetObject($prtForm);
+		$dataObj = $prtFormObj->getDataObj();
+		$parent_record_table = $dataObj->m_MainTable;
+		
+		$aclDO = BizSystem::getObject("common.do.DataACLDO");
+		$sql = "
+			[record_table]='$parent_record_table' AND 
+			[record_id]='$parent_record_id' AND
+			[user_id]='$acl_user_id' 
+		";
+		$rec = $aclDO->fetchOne($sql);
+		
+		if(!$rec){		
+			$aclRecord = array(
+				"record_table" => $parent_record_table,
+				"record_id" => $parent_record_id,
+				"user_id"	=> $acl_user_id,
+				"user_perm"	=> $acl_perm
+			);
+			$aclDO->insertRecord($aclRecord);
+		}
+		
+		$this->rerender();
+	}
+	
+	
 }
 ?>
