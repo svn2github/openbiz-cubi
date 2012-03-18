799a7a0d45efca09dedaf640d2ed3b492d628744
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-18 05:28:07 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2651 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/repository/websvc/PackageListService.php
@@ -0,0 +1,64 @@
+<?php
+
+include_once MODULE_PATH.'/websvc/lib/WebsvcService.php';
+
+class PackageListService extends WebsvcService
+{
+    protected $packageMasterDO = "packagemstr.master.do.PackageMasterDO";
+    protected $packageCategoryDO = "packagemstr.category.do.PackageCategoryDO";
+    
+    public function foo($args)
+    {
+        return 'ok';
+    }
+    
+    public function invoke()
+    {
+        /*
+        $username = $this->getInput('username');
+        $api_key = $this->getInput('api_key');
+        $secret = $this->getInput('secret');
+        $format = $this->getInput('format');
+        if ($this->authenticate($username, $api_key, $secret) == false) {
+            $this->output(null, $format);
+            return;
+        }
+        */
+        $format = $this->getInput('format');
+        $service = $this->getInput('service');
+        $method = $this->getInput('method');
+        if ($this->checkAccess($username, $method) == false) {
+            $this->output(null, $format);
+            return;
+        }
+        
+        // read inputs
+        $args = $this->getInputArgs('args');
+        
+        // call function
+        $response = $this->$method($args);
+                
+        $this->output($response, $format);
+    }
+    
+    public function search($args)
+    {
+        $searchRule = $args['searchrule'];
+        $limit = $args['limit'];
+        $packageMasterDO = BizSystem::getObject($this->packageMasterDO);
+        $dataSet = $packageMasterDO->directFetch($searchRule,$limit);
+        $resultArray = $dataSet->toArray();
+        return $resultArray;
+    }
+    
+    public function list_categories($args)
+    {
+        $searchRule = $args['searchrule'];
+        $limit = $args['limit'];
+        $packageCategoryDO = BizSystem::getObject($this->packageCategoryDO);
+        $dataSet = $packageCategoryDO->directFetch($searchRule,$limit);
+        $resultArray = $dataSet->toArray();
+        return $resultArray;
+    }
+}
+?>
