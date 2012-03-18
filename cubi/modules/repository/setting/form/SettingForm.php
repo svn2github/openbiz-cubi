357b766d06e61714712d5453ee7bdfe63f7359e3
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-18 08:00:28 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2658 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/repository/setting/form/SettingForm.php
@@ -0,0 +1,158 @@
+<?php 
+/**
+ * Openbiz Cubi 
+ *
+ * LICENSE
+ *
+ * This source file is subject to the BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @package   user.form
+ * @copyright Copyright (c) 2005-2011, Rocky Swen
+ * @license   http://www.opensource.org/licenses/bsd-license.php
+ * @link      http://www.phpopenbiz.org/
+ * @version   $Id$
+ */
+
+
+/**
+ * AccountEditForm class - implement the logic of edit my account form
+ *
+ * @package user.form
+ * @author Rocky Swen
+ * @copyright Copyright (c) 2005-2009
+ * @access public
+ */
+class SettingForm extends EasyForm
+{
+    protected $_userId = null;
+    
+    function __construct(&$xmlArr)
+    {
+        parent::__construct($xmlArr);        
+        $this->_userId = BizSystem::getUserProfile("Id");
+    }
+    
+    public function allowAccess(){
+    	parent::allowAccess();
+
+    	if(BizSystem::getUserProfile("Id"))
+    	{
+  	 		return 1;
+    	}
+    	else
+    	{
+    		return 0;
+    	}
+    }    
+    
+    public function fetchData(){
+        if ($this->m_ActiveRecord != null)
+            return $this->m_ActiveRecord;
+        
+        $dataObj = $this->getDataObj();
+        if ($dataObj == null) return;
+
+		
+        if (!$this->m_FixSearchRule && !$this->m_SearchRule)
+        	return array();
+        
+    	QueryStringParam::setBindValues($this->m_SearchRuleBindValues);
+        
+        	
+        if ($this->m_RefreshData)   $dataObj->resetRules();
+        else $dataObj->clearSearchRule();
+
+        if ($this->m_FixSearchRule)
+        {
+            if ($this->m_SearchRule)
+                $searchRule = $this->m_SearchRule . " AND " . $this->m_FixSearchRule;
+            else
+                $searchRule = $this->m_FixSearchRule;
+        }
+
+        $dataObj->setSearchRule($searchRule);
+        QueryStringParam::setBindValues($this->m_SearchRuleBindValues);        
+
+        $resultRecords = $dataObj->fetch();
+        foreach($resultRecords as $record){
+        	$prefRecord["_".$record['name']] = $record["value"];
+        }
+        
+        $this->m_RecordId = $resultRecords[0]['Id'];
+        $this->setActiveRecord($prefRecord);
+
+        QueryStringParam::ReSet();
+
+        return $prefRecord;    
+    }
+    
+    public function updateRecord()
+    {
+        $currentRec = $this->fetchData();
+        $recArr = $this->readInputRecord();
+
+        if (count($recArr) == 0)
+            return;
+
+        try
+        {
+            $this->ValidateForm();
+        }
+        catch (ValidationException $e)
+        {
+            $this->processFormObjError($e->m_Errors);
+            return;
+        }
+		
+        // new save logic
+        $user_id = BizSystem::getUserProfile("Id");
+        $prefDo = $this->getDataObj();
+        
+        foreach ($this->m_DataPanel as $element)
+        {
+            $value = $recArr[$element->m_FieldName];
+            if ($value === null){ 
+            	continue;
+            } 
+            if(substr($element->m_FieldName,0,1)=='_'){
+	            $name = substr($element->m_FieldName,1);
+            	$recArrParam = array(
+            		"user_id" => $user_id,
+            		"name"	  => $name,
+            		"value"   => $value,
+	            	"section" => $element->m_ElementSetCode,
+	            	"type" 	  => $element->m_Class,	            
+	            );
+	            //check if its exsit
+	            $record = $prefDo->fetchOne("[user_id]='$user_id' and [name]='$name'");
+	            if($record){
+	            	//update it
+	            	$recArrParam["Id"] = $record->Id;
+	            	$prefDo->updateRecord($recArrParam,$record->toArray());
+	            }else{
+	            	//insert it	            	
+	            	$prefDo->insertRecord($recArrParam);
+	            }
+            }
+        }
+        
+		//reload profile
+		BizSystem::getService(PROFILE_SERVICE)->InitProfile(BizSystem::getUserProfile("username"));
+        
+
+        // in case of popup form, close it, then rerender the parent form
+        if ($this->m_ParentFormName)
+        {
+            $this->close();
+
+            $this->renderParent();
+        }
+
+        $this->processPostAction();
+
+    }
+
+
+}  
+?>
