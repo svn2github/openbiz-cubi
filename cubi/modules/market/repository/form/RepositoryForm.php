1afbeecf861e7dfd548596ea5ee5958094459a9b
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-19 06:37:07 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2671 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/market/repository/form/RepositoryForm.php
+++ cubi/modules/market/repository/form/RepositoryForm.php
@@ -1,6 +1,14 @@
 <?php 
 class RepositoryForm extends EasyForm
 {
+	public $m_RepoValidated = 'N';
 	
+	public function checkRepo()
+	{
+		$rec= $this->readInputRecord();
+		$repo_uri = $rec['repository_uri'];
+		$this->m_RepoValidated = 'Y';
+		$this->rerender();
+	}
 }
 ?>
