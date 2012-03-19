f5840f22d72d3f00826c121ea27457a6350cba90
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-19 09:24:38 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2687 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/repository/websvc/RepositoryService.php
+++ cubi/modules/repository/websvc/RepositoryService.php
@@ -4,67 +4,13 @@ include_once MODULE_PATH.'/websvc/lib/WebsvcService.php';
 
 class RepositoryService extends WebsvcService
 {
-    protected $packageMasterDO = "packagemstr.master.do.PackageMasterDO";
-    protected $packageCategoryDO = "packagemstr.category.do.PackageCategoryDO";
-    
-    public function foo($args)
-    {
-        return 'ok';
-    }
-    
-    public function invoke()
-    {
-        /*
-        $username = $this->getInput('username');
-        $api_key = $this->getInput('api_key');
-        $secret = $this->getInput('secret');
-        $format = $this->getInput('format');
-        if ($this->authenticate($username, $api_key, $secret) == false) {
-            $this->output(null, $format);
-            return;
-        }
-        */
-        $format = $this->getInput('format');
-        $service = $this->getInput('service');
-        $method = $this->getInput('method');
-        if ($this->checkAccess($username, $method) == false) {
-            $this->output(null, $format);
-            return;
-        }
-        
-        // read inputs
-        $args = $this->getInputArgs('args');
-        
-        // call function
-        $response = $this->$method($args);
-                
-        $this->output($response, $format);
-    }
-    
-    public function search($args)
-    {
-        $searchRule = $args['searchrule'];
-        $limit = $args['limit'];
-        $packageMasterDO = BizSystem::getObject($this->packageMasterDO);
-        $dataSet = $packageMasterDO->directFetch($searchRule,$limit);
-        $resultArray = $dataSet->toArray();
-        return $resultArray;
-    }
-    
-    public function list_categories($args)
-    {
-        $searchRule = $args['searchrule'];
-        $limit = $args['limit'];
-        $packageCategoryDO = BizSystem::getObject($this->packageCategoryDO);
-        $dataSet = $packageCategoryDO->directFetch($searchRule,$limit);
-        $resultArray = $dataSet->toArray();
-        return $resultArray;
-    }
-    
+	protected $m_ApplicationDO = "repository.application.do.ApplicationDO";
+	protected $m_RepositorySettingDO = "myaccount.do.PreferenceDO";
+	
     public function fetchRepoInfo()
     {
-    	$dataObj = BizSystem::getObject("myaccount.do.PreferenceDO");
     	$searchRule = "[user_id]='0' AND [name] LIKE 'repo_%'";   
+    	$dataObj = BizSystem::getObject($this->m_RepositorySettingDO);    	
         $resultRecords = $dataObj->directfetch($searchRule);
         $prefRecord = array();
         foreach($resultRecords as $record){
@@ -72,5 +18,14 @@ class RepositoryService extends WebsvcService
         }
         return $prefRecord;
     }
+
+    public function fetchFeaturedApps()
+    {
+    	$searchRule = "[status]=1 AND [release_time] < NOW() AND [featured]=1";
+    	$dataObj = BizSystem::getObject($this->m_ApplicationDO);    	   
+        $resultRecords = $dataObj->directfetch($searchRule);       
+        return $resultRecords;
+    }    
+       
 }
 ?>
