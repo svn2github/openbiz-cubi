acb6945616c805c3fc28f79fe0d44219fc8b2407
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-19 03:36:17 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2665 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/market/mod.install.sql
+++ cubi/modules/market/mod.install.sql
@@ -1,18 +1,26 @@
-/*
-market_repository
-id
-repository_uri
-repository_uid
-status
-create_by
-create_time
 
+DROP TABLE IF EXISTS `market_repository`;
+CREATE TABLE IF NOT EXISTS `market_repository` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `repository_uri` varchar(255) NOT NULL,
+  `repository_uid` varchar(255) NOT NULL,
+  `status` int(2) NOT NULL,
+  `create_by` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `repository_uid` (`repository_uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-market_installed
-repository_uid
-app_id
-version
-install_time
-create_by
-create_time
-*/
+DROP TABLE IF EXISTS `market_installed`;
+CREATE TABLE IF NOT EXISTS `market_installed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `repository_uid` varchar(255) NOT NULL,
+  `app_id` int(11) NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `install_time` datetime NOT NULL,
+  `create_by` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `repository_uid` (`repository_uid`,`app_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
