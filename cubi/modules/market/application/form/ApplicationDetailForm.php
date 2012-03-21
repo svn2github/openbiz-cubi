9535e8b4417b5536afa44d9f4a7e26ee54705255
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-21 11:25:00 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2735 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/market/application/form/ApplicationDetailForm.php
+++ cubi/modules/market/application/form/ApplicationDetailForm.php
@@ -8,14 +8,19 @@ class ApplicationDetailForm extends AppListForm
 		$repo_uri = $this->getDefaultRepoURI();
 		
 		$svc = BizSystem::getService("market.lib.PackageService");
+		$util = BizSystem::getService(UTIL_SERVICE);
+		
 		$appInfo = $svc->discoverAppInfo($repo_uri,$app_id);
 		$this->m_RecordId = $appInfo['Id'];
 		$appInfo['icon'] = $repo_uri.$appInfo['icon'];
 		
 		$releaseInfo = $svc->discoverAppLatestRelease($repo_uri,$app_id);
-		$releaseInfo['url']= $repo_uri.$releaseInfo['url'];
-		
-		$appInfo['latest_release'] = $releaseInfo;
+		if($releaseInfo){
+			$releaseInfo['url']= $repo_uri.$releaseInfo['url'];
+			$releaseInfo['create_date'] = date("Y-m-d",strtotime($releaseInfo['create_time']));
+			$releaseInfo['filesize'] = $util->format_bytes($releaseInfo['filesize']);
+			$appInfo['latest_release'] = $releaseInfo;
+		}
 		return $appInfo;
 	}
 }
