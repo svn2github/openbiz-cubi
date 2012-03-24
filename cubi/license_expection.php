db5bbab1ef23c921d661651318b22d478fdabbf5
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-24 10:07:40 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2774 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/license_expection.php
+++ cubi/license_expection.php
@@ -2,9 +2,10 @@
 function ioncube_event_handler($err_code, $params)
 {
 	$formObj = BizSystem::getObject("common.form.LicenseInvalidForm");
+	$formObj->m_SourceURL = $_SERVER['REQUEST_URI'];
 	$formObj->m_ErrorCode = $err_code;
 	$formObj->m_ErrorParams = $params;
-	$viewObj = BizSystem::getObject("common.view.LicenseInvalidView");
+	$viewObj = BizSystem::getObject("common.view.LicenseInvalidView");	
 	$viewObj->render();	
 	exit;
 }
