b31b5aabf4db47d0d8d07c19dde09a50a1ca3555
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-26 07:21:41 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2782 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/market/lib/InstallerService.php
+++ cubi/modules/market/lib/InstallerService.php
@@ -1,46 +1,46 @@
 <?php 
-class InstallerService
-{
+require_once "PackageService.php";
 
-    public function downloadPackage($package, $install=true)
-    {
-        $this->_installPackage = $package;
-        // get the master package record
-        $pkgs = $this->findPackages("[status]=1 AND [name]='$package'");
-        if (!$pkgs) { 
+class InstallerService extends PackageService
+{
+	
+	protected $_installPackage = "";
+	const INSTALLED_DO = "market.installed.do.InstalledDO";
+	
+    public function downloadPackage($uri, $app_id)
+    {        
+        $pkg = $this->discoverAppLatestRelease($uri,$app_id);
+        
+        if (!$pkg) { 
             return null;
         }
-        //print_r($pkgs);
-        
-        // download cpk from respository url
-        $pkg = $pkgs['data'][0];
- 		// this is just for test download bar
- 		// $pkg['repository']='http://guides.hosting.czm.cn/test.rar';
         if (empty($pkg)) {
-            $this->setInstallInfo($package, array("state"=>"ERROR","log"=>"Unable to find same package from repository"));
-            pkg_log("Package download error: Unable to find same package $package from repository");
+            $this->setInstallInfo($pkg, array("state"=>"ERROR","log"=>"Unable to find same package from repository"));
+            $this->pkg_log("Package download error: Unable to find same package from repository");
             return false;
         }
-        pkg_log(">>> Package: ".$pkg['package_id'].", ".$pkg['name'].", ".$pkg['version'].", ".$pkg['repository']."\n");
+        $this->pkg_log(">>> Package: ".$pkg['app_id'].", ".$pkg['filename'].", ".$pkg['version'].", ".$pkg['url']."\n");
         
+        $this->_installPackage = $pkg;
         try {
-            $this->setInstallInfo($package, array("version"=>$pkg['version'],"state"=>"Start"));
-            
-            // save cpk to tmp folder           
-            $url = $pkg['repository'];
+            $this->setInstallInfo($pkg, array("version"=>$pkg['version'],"state"=>"Start"));
+    	           
+            $url = $uri.$pkg['url'];
             if (empty($url)) {
                 $this->setInstallInfo($package, array("state"=>"ERROR","log"=>"Unable to install from empty url."));
                 return false;
             }
-            $filename = APP_HOME."/files/tmpFiles/".basename($pkg['repository']);
-            pkg_log("Downloading from $url ...\n");
-            $this->downloadUrl($url, $filename);
-            pkg_log("Completed download from $url ...\n");
-            
-            if ($install) {
-                // install cpk 
-                $this->installPackage($filename);
+            $file_dir = APP_FILE_PATH.DIRECTORY_SEPARATOR."tmpFiles".DIRECTORY_SEPARATOR;
+            if(!is_dir($file_dir)){
+	            mkdir($file_dir,0777);
             }
+            $filename = $file_dir.basename($pkg['url']);
+            $this->pkg_log("Downloading from $url ...\n");
+            $this->downloadUrl($url, $filename);
+            $this->pkg_log("Completed download from $url ...\n");
+
+            $this->installPackage($filename);
+          
             
             if (file_exists($filename)) {
                 return $filename;
@@ -54,12 +54,200 @@ class InstallerService
             $this->setInstallInfo($package, array("state"=>"ERROR","log"=>$msg));
         }
     }	
+    
+
+	protected function downloadUrl($url, $filename)
+    {
+    	$package = $this->_installPackage;
+        
+        //clean setting for counting download, filesize, download size
+    	$this->setInstallInfo($package, array("filesize"=>"0" , "download"=>"0"));
+    	
+    	$filesize = $this->_remote_filesize($url);
+    	$this->setInstallInfo($package, array("filesize"=>$filesize , "download"=>"0"));
+    	
+        $this->setInstallInfo($package, array("state"=>"Download","log"=>"Downloading 0..."));
+        $handle = fopen($url, "rb");
+        if (!$handle) {
+            $this->setInstallInfo($package, array("state"=>"ERROR","log"=>"Unable to open url $url"));
+            throw new Exception("Unable to open url $url");
+        }
+        $fp = fopen($filename, "wb");
+        if (!$fp) {
+            $this->setInstallInfo($package, array("state"=>"ERROR","log"=>"nable to open file $filename"));
+            throw new Exception("Unable to open file $filename");
+        }
+        $total = 0;
+        $contents = '';
+        $utilService = BizSystem::getService("service.utilService");
+        while (!feof($handle)) {
+            $contents = fread($handle, 8192);
+            $result = fwrite($fp, $contents);
+            $total += strlen($contents);
+            $this->setInstallInfo($package, array("log"=>"Downloading ".$utilService->format_bytes($total)." of ".$utilService->format_bytes($filesize)." ...","filesize"=>$filesize ,"download"=>$total));
+            $this->pkg_log("Downloading ".$utilService->format_bytes($total)." of ".$utilService->format_bytes($filesize)."\n");
+            //sleep(2);
+        }
+        fclose($handle);
+        fclose($fp);
+    }      
+    
+    
+    protected function _remote_filesize($url, $user = "", $pw = "")
+    {
+	    ob_start();
+	    $ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_HEADER, 1);
+	    curl_setopt($ch, CURLOPT_NOBODY, 1);
+	
+	    if(!empty($user) && !empty($pw))
+	    {
+	    $headers = array('Authorization: Basic ' .  base64_encode("$user:$pw"));
+	    	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	    }
+	
+	    $ok = curl_exec($ch);
+	    curl_close($ch);
+	    $head = ob_get_contents();
+	    ob_end_clean();
+	
+	        	
+	    $regex = '/Content-Length:\s([0-9]+)/si';
+	    $count = preg_match($regex, $head, $matches);
+	    return isset($matches[1])?$matches[1]:'unknown';
+    }    
+    
+
+    protected function _unpack($tarfile, $toFolder)
+    {
+        // include PEAR Tar class
+        set_include_path(get_include_path(). PATH_SEPARATOR . APP_HOME."/bin/phing/classes");
+        include_once(APP_HOME."/bin/phing/classes/Archive/Tar.php");
+        if (!class_exists('Archive_Tar')) {
+            throw new Exception("You must have installed the PEAR Archive_Tar class in order to use UntarTask.");
+        }
+        $tar = $this->_initTar($tarfile);
+        $result = $tar->extract($toFolder);
+        if (!$result) {
+            throw new Exception("Could not extract tar file: $tarfile");
+        }
+    }    
+    
+    protected function _filecopy($tmpFolder)
+    {
+        $dir0s = ob_scandir($tmpFolder);
+        // copy module to cubi/upgrade folder
+        foreach ($dir0s as $dir0) {
+            $dirs = ob_scandir($tmpFolder."/$dir0");
+            foreach ($dirs as $dir) {
+                $srcDir = $tmpFolder."/$dir0/$dir";
+                $dstDir = ($dir == 'modules') ? APP_HOME."/upgrade/modules" : APP_HOME."/$dir";
+                $this->pkg_log("copy $srcDir to $dstDir \n");
+                recurse_copy($srcDir, $dstDir);
+                if ($dir == 'modules')
+                    $this->_installModules = ob_scandir($srcDir);
+            }
+        }    
+    }
+        
+    private function _initTar($tarfile)
+    {
+        $compression = null;
+        $tarfileName = basename($tarfile);
+        $mode = strtolower(substr($tarfileName, strrpos($tarfileName, '.')));
+        if ($mode == ".cpk")
+            $mode = ".gz";
+
+        $compressions = array(
+                'gz' => array('.gz', '.tgz',),
+                'bz2' => array('.bz2',),
+            );
+        foreach ($compressions as $algo => $ext) {
+            if (array_search($mode, $ext) !== false) {
+                $compression = $algo;
+                break;
+            }
+        }
+        return new Archive_Tar($tarfile, $compression);
+    }    
+    
+    public function installPackage($cpkFilePath)
+    {   
+        $package = $this->_installPackage;
+        $this->setInstallInfo($package, array("state"=>"Install","log"=>"Installing ..."));
+
+        $tmpFolder = APP_FILE_PATH.DIRECTORY_SEPARATOR."tmpFiles".DIRECTORY_SEPARATOR;
+        $toFolder = $tmpFolder.time();
+ 
+        try {
+            $this->_unpack($cpkFilePath, $toFolder);
+        }
+        catch (Exception $e) {
+            throw new Exception("ERROR in unpack $cpkFilePath. ".$e->getMessage());
+        }
+        $this->pkg_log("Unpack. $cpkFilePath is unpacked to $toFolder\n");
+        
+        // copy files to target folder from the tmp folder
+        $this->_filecopy($toFolder);
+        
+        // invoke module upgrade command
+        foreach ($this->_installModules as $moduleName) {
+            $this->pkg_log("invoke module upgrade command\n");        
+            $loader = new ModuleLoader($moduleName);
+            $loader->debug = 0;
+            $this->pkg_log("Start upgrading $moduleName module ...".PHP_EOL);
+            $this->pkg_log("--------------------------------------------------------".PHP_EOL);
+            $result = $loader->upgradeModule();
+            $this->pkg_log($loader->errors . "".PHP_EOL);
+            $this->pkg_log($loader->logs . "".PHP_EOL);
+            if ($result == false) {
+                throw new Exception("Error in install package. ".$loader->errors);
+            }
+            
+            // load the module again
+            $this->pkg_log(PHP_EOL."Reload module ...".PHP_EOL);
+            $loader->loadModule($installSql);
+            $this->pkg_log($loader->errors . "".PHP_EOL);
+        }
+        $time = date('Y-m-d H:i:s');
+        $this->setInstallInfo($package, array("time"=>$time,"state"=>"OK","log"=>"Completed"));
+    }    
+    
+    protected function setInstallInfo($pkgArr,$installInfo)
+    {	                                	
+        $pkgDo = BizSystem::GetObject(self::INSTALLED_DO);        
+        $dataRec = $pkgDo->fetchOne("[app_id]='".$pkgArr['app_id']."'");        
+        
+        if (!$dataRec){        	
+        	$dataRec = new DataRecord(null,$pkgDo);
+        	$dataRec["app_id"] = $pkgArr['app_id'];
+        	        	
+        }
+				
+        if (isset($installInfo['version'])) $dataRec["install_version"] = $installInfo['version'];
+        if (isset($installInfo['time'])) $dataRec["install_time"] = $installInfo['time'];
+        if (isset($installInfo['state'])) $dataRec["install_state"] = $installInfo['state'];
+        if (isset($installInfo['log'])) $dataRec["install_log"] = $installInfo['log'];
+
+        if (isset($installInfo['filesize'])) $dataRec["install_download_filesize"] = $installInfo['filesize'];
+        if (isset($installInfo['download'])) $dataRec["install_download"] = $installInfo['download'];
+
+        try {
+            $dataRec->save();            
+        }
+        catch (Exception $e) {
+            throw new Exception("setInstallInfo. Unable to save the record. ".$e->getMessage());
+        }
+        return true;
+    }    
+    
+      
 	
 	function pkg_log($text)
 	{
 	    if (CLI) echo $text.nl;
 	    //BizSystem::log(LO_ERR, "ECHO", $text);
-	    $logfile = LOG_PATH."/install_pkg.log";
+	    $logfile = LOG_PATH."/INSTALL_PKG.log";
 	    $fp = fopen($logfile, "a+");
 	    fwrite($fp, date('c')." ".$text);
 	    fclose($fp);
@@ -73,12 +261,12 @@ class InstallerService
      * ==================================================================================
      * */
 	
-    protected $_installPackage = "";
+    
     protected $_installModules = array();
     
     public $m_CacheLifeTime = null;	
     
-    const LOCAL_PACKAGE_DO = "package.local.do.PackageLocalDO";
+    
 
     public $repositoryUrl; // repository url
     
@@ -216,28 +404,7 @@ class InstallerService
         }
     }
     
-    protected function setInstallInfo($pkgname, $installInfo)
-    {
-        $pkgDo = BizSystem::GetObject(self::LOCAL_PACKAGE_DO);
-        $dataRec = $pkgDo->fetchByName($pkgname);
-        if (!$dataRec) return false;
 
-        if (isset($installInfo['version'])) $dataRec["inst_version"] = $installInfo['version'];
-        if (isset($installInfo['time'])) $dataRec["inst_time"] = $installInfo['time'];
-        if (isset($installInfo['state'])) $dataRec["inst_state"] = $installInfo['state'];
-        if (isset($installInfo['log'])) $dataRec["inst_log"] = $installInfo['log'];
-
-        if (isset($installInfo['filesize'])) $dataRec["inst_filesize"] = $installInfo['filesize'];
-        if (isset($installInfo['download'])) $dataRec["inst_download"] = $installInfo['download'];
-        //BizSystem::log(LOG_DEBUG, "PackageService", "save to db ".print_r($installInfo,1));
-        try {
-            $dataRec->save();
-        }
-        catch (Exception $e) {
-            throw new Exception("setInstallInfo. Unable to save the record. ".$e->getMessage());
-        }
-        return true;
-    }
     
     protected function getInstallInfo($pkgname)
     {
@@ -300,146 +467,11 @@ class InstallerService
         return true;
     }
     
-    protected function downloadUrl($url, $filename)
-    {
-    	$package = $this->_installPackage;
-        
-        //clean setting for counting download, filesize, download size
-    	$this->setInstallInfo($package, array("filesize"=>"0" , "download"=>"0"));
-    	
-    	$filesize = $this->remote_filesize($url);
-    	$this->setInstallInfo($package, array("filesize"=>$filesize , "download"=>"0"));
-    	
-        $this->setInstallInfo($package, array("state"=>"Download","log"=>"Downloading 0..."));
-        $handle = fopen($url, "rb");
-        if (!$handle) {
-            $this->setInstallInfo($package, array("state"=>"ERROR","log"=>"Unable to open url $url"));
-            throw new Exception("Unable to open url $url");
-        }
-        $fp = fopen($filename, "wb");
-        if (!$fp) {
-            $this->setInstallInfo($package, array("state"=>"ERROR","log"=>"nable to open file $filename"));
-            throw new Exception("Unable to open file $filename");
-        }
-        $total = 0;
-        $contents = '';
-        $utilService = BizSystem::getService("service.utilService");
-        while (!feof($handle)) {
-            $contents = fread($handle, 8192);
-            $result = fwrite($fp, $contents);
-            $total += strlen($contents);
-            $this->setInstallInfo($package, array("log"=>"Downloading ".$utilService->format_bytes($total)." of ".$utilService->format_bytes($filesize)." ...","filesize"=>$filesize ,"download"=>$total));
-            pkg_log("Downloading ".$utilService->format_bytes($total)." of ".$utilService->format_bytes($filesize)."\n");
-            //sleep(2);
-        }
-        fclose($handle);
-        fclose($fp);
-    }
     
-    protected function remote_filesize($url, $user = "", $pw = "")
-    {
-	    ob_start();
-	    $ch = curl_init($url);
-	    curl_setopt($ch, CURLOPT_HEADER, 1);
-	    curl_setopt($ch, CURLOPT_NOBODY, 1);
-	
-	    if(!empty($user) && !empty($pw))
-	    {
-	    $headers = array('Authorization: Basic ' .  base64_encode("$user:$pw"));
-	    	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	    }
-	
-	    $ok = curl_exec($ch);
-	    curl_close($ch);
-	    $head = ob_get_contents();
-	    ob_end_clean();
-	
-	        	
-	    $regex = '/Content-Length:\s([0-9]+)/si';
-	    $count = preg_match($regex, $head, $matches);
-	    return isset($matches[1])?$matches[1]:'unknown';
-    }    
 
-    /*
-        phing unzip package
-        upgrader.php to install package
-            backup, copy, sql upgrade
-            load module
-    */
-    public function installPackage($cpkFilePath)
-    {
-        $package = $this->_installPackage;
-        $this->setInstallInfo($package, array("state"=>"Install","log"=>"Installing ..."));
-        // tmp folder
-        $tmpFolder = APP_HOME."/files/tmpfiles";
-        $toFolder = $tmpFolder."/".time();
-        // unzip cpk file
-        try {
-            $this->unpack($cpkFilePath, $toFolder);
-        }
-        catch (Exception $e) {
-            throw new Exception("ERROR in unpack $cpkFilePath. ".$e->getMessage());
-        }
-        pkg_log("Unpack. $cpkFilePath is unpacked to $toFolder\n");
-        
-        // copy files to target folder from the tmp folder
-        $this->filecopy($toFolder);
-        
-        // invoke module upgrade command
-        foreach ($this->_installModules as $moduleName) {
-            pkg_log("invoke module upgrade command\n");        
-            $loader = new ModuleLoader($moduleName);
-            $loader->debug = 0;
-            pkg_log("Start upgrading $moduleName module ...".PHP_EOL);
-            pkg_log("--------------------------------------------------------".PHP_EOL);
-            $result = $loader->upgradeModule();
-            pkg_log($loader->errors . "".PHP_EOL);
-            pkg_log($loader->logs . "".PHP_EOL);
-            if ($result == false) {
-                throw new Exception("Error in install package. ".$loader->errors);
-            }
-            
-            // load the module again
-            pkg_log(PHP_EOL."Reload module ...".PHP_EOL);
-            $loader->loadModule($installSql);
-            pkg_log($loader->errors . "".PHP_EOL);
-        }
-        $time = date('Y-m-d H:i:s');
-        $this->setInstallInfo($package, array("time"=>$time,"state"=>"OK","log"=>"Completed"));
-    }
+
     
-    protected function filecopy($tmpFolder)
-    {
-        $dir0s = ob_scandir($tmpFolder);
-        // copy module to cubi/upgrade folder
-        foreach ($dir0s as $dir0) {
-            $dirs = ob_scandir($tmpFolder."/$dir0");
-            foreach ($dirs as $dir) {
-                $srcDir = $tmpFolder."/$dir0/$dir";
-                $dstDir = ($dir == 'modules') ? APP_HOME."/upgrade/modules" : APP_HOME."/$dir";
-                pkg_log("copy $srcDir to $dstDir \n");
-                recurse_copy($srcDir, $dstDir);
-                if ($dir == 'modules')
-                    $this->_installModules = ob_scandir($srcDir);
-            }
-        }
-        //print_r($this->_installModules);
-    }
-    
-    protected function unpack($tarfile, $toFolder)
-    {
-        // include PEAR Tar class
-        set_include_path(get_include_path(). PATH_SEPARATOR . APP_HOME."/bin/phing/classes");
-        include_once(APP_HOME."/bin/phing/classes/Archive/Tar.php");
-        if (!class_exists('Archive_Tar')) {
-            throw new Exception("You must have installed the PEAR Archive_Tar class in order to use UntarTask.");
-        }
-        $tar = $this->initTar($tarfile);
-        $result = $tar->extract($toFolder);
-        if (!$result) {
-            throw new Exception("Could not extract tar file: $tarfile");
-        }
-    }
+
     
     /**
      * Init a Archive_Tar class with correct compression for the given file.
@@ -447,27 +479,7 @@ class InstallerService
      * @param $tarfile
      * @return Archive_Tar the tar class instance
      */
-    private function initTar($tarfile)
-    {
-        $compression = null;
-        $tarfileName = basename($tarfile);
-        $mode = strtolower(substr($tarfileName, strrpos($tarfileName, '.')));
-        if ($mode == ".cpk")
-            $mode = ".gz";
 
-        $compressions = array(
-                'gz' => array('.gz', '.tgz',),
-                'bz2' => array('.bz2',),
-            );
-        foreach ($compressions as $algo => $ext) {
-            if (array_search($mode, $ext) !== false) {
-                $compression = $algo;
-                break;
-            }
-        }
-        //pkg_log("tarfilename is $tarfileName, mode is $mode, $compression \n");
-        return new Archive_Tar($tarfile, $compression);
-    }
 }
 
 
