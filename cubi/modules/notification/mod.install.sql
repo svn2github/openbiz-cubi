d1cafdba8783c0d1822b6134744f74c241e73597
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-28 07:01:47 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2853 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/notification/mod.install.sql
+++ cubi/modules/notification/mod.install.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `notification`;
 CREATE TABLE IF NOT EXISTS `notification` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
   `message` text NOT NULL,
   `goto_url` varchar(255) NOT NULL,
   `read_state` int(11) NOT NULL,
--- cubi/modules/notification/widget/NotificationWidgetForm.php
+++ cubi/modules/notification/widget/NotificationWidgetForm.php
@@ -1,6 +1,25 @@
 <?php 
 class NotificationWidgetForm extends EasyForm
 {
+	public function render()
+	{
+		$result = parent::render();
+		if(!$this->m_TotalRecords)
+		{
+			return "";
+		}
+		return $result;
+	}
 	
+	public function fetchDataSet()
+	{
+		$resultSet = parent::fetchDataSet();
+		foreach($resultSet as $key => $record)
+		{
+			$record['release_date'] = date("Y-m-d",strtotime($record['create_time']));
+			$resultSet[$key] = $record;
+		}
+		return $resultSet;
+	}
 }
 ?>
