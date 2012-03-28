fe55cc37dc8b563a52ce4be1995d43ab59bfa69f
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-03-28 05:34:54 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2847 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/notification/mod.install.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `notification`; 
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `goto_url` varchar(255) NOT NULL,
+  `read_state` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
