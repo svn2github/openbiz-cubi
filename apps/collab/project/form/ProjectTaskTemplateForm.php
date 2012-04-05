86ce8d311a17b2f7eec54378f89b20bcd60190d7
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-05 06:22:29 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2911 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/collab/project/form/ProjectTaskTemplateForm.php
+++ apps/collab/project/form/ProjectTaskTemplateForm.php
@@ -11,7 +11,7 @@ class ProjectTaskTemplateForm extends EasyForm
 			$taskTempDO = BizSystem::getObject("collab.project.do.ProjectTaskTemplateDO",1);
 			$dependTaskRec = $taskTempDO->fetchById($recArr['dependency_task_id']);
 			$earist_start_date = $dependTaskRec['start_date'] + $dependTaskRec['during_days'];
-			if($recArr['start_date']<=$earist_start_date)
+			if($recArr['start_date']<$earist_start_date)
 			{
 				$errorMessage = $this->getMessage("START_TIME_TOO_EARLY",array($earist_start_date+1));                                
                 $this->m_ValidateErrors['fld_start_time'] = $errorMessage;
