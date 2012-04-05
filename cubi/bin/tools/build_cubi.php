4bdd4d9331036f72c855baccafc95155adb3a8dd
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-05 07:16:28 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2912 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/bin/tools/build_cubi.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once ("../app_init.php");
+if(!defined("CLI")){
+	exit;
+}
+
+$moduleName = $argv[1];
+
+
+// invoke cubi/build/build mod_build.xml -Dbuild.module=$moduleName -Dbuild.number=$buildNumber
+echo "---------------------------------------\n";
+execPhing("cubi.xml");
+
+function execPhing($buildFile, $options=null)
+{
+    $phingHome = APP_HOME.DIRECTORY_SEPARATOR."bin".DIRECTORY_SEPARATOR."phing";
+    putenv("PHING_HOME=$phingHome");
+    $phpClasses = $phingHome.DIRECTORY_SEPARATOR."classes";
+    putenv("PHP_CLASSPATH=$phpClasses");
+    $phingBin = $phingHome.DIRECTORY_SEPARATOR."bin";
+    //putenv("PATH=$phingBin");
+    $cmd = $phingBin.DIRECTORY_SEPARATOR."phing"." -buildfile $buildFile $options";
+    echo "Executing $cmd\n";
+    chdir(APP_HOME.DIRECTORY_SEPARATOR."build");
+    system($cmd);
+}
+
+
+?>
