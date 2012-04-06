b1df2c460ca319283bf7696a4edaba6c2784c05a
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-06 07:32:01 +0000

    add license client service.
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2928 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/service/licenseClient.php
@@ -0,0 +1,44 @@
+<?php 
+include_once(MODULE_PATH."/common/lib/fileUtil.php");
+include_once(MODULE_PATH."/common/lib/httpClient.php");
+
+class LicenseClient extends MetaObject
+{
+    protected $_installPackage = "";
+    protected $_installModules = array();
+    
+    public $m_CacheLifeTime = null;	
+    
+    public $repositoryUrl; // repository url
+    
+    function __construct(&$xmlArr)
+    {
+        $this->readMetadata($xmlArr);
+    } 
+       	
+    protected function readMetadata(&$xmlArr)
+    {
+        parent::readMetadata($xmlArr);
+    	$this->repositoryUrl = isset($xmlArr["PLUGINSERVICE"]["ATTRIBUTES"]["REPOSITORYURL"]) ? $xmlArr["PLUGINSERVICE"]["ATTRIBUTES"]["REPOSITORYURL"] : "";
+    }
+    
+    public function acquireLicense($activationCode, $contactEmail, $serverData)
+    {
+        //try to process cache service.
+        $argsJson = json_encode(array("activation_code"=>$activationCode,"contact_email"=>$contactEmail,"server_data"=>$serverData));
+        $query = array(	"method=acquireLicense","format=json",
+                        "argsJson=$argsJson");
+        $httpClient = new HttpClient('POST');
+        foreach ($query as $q)
+            $httpClient->addQuery($q);
+        $headerList = array();
+        $out = $httpClient->fetchContents($this->repositoryUrl, $headerList);
+        echo $out;
+        $lic = json_decode($out, true);
+        $licenseArray = $lic['data'];
+
+        return $licenseArray;
+    }
+}
+
+?>
