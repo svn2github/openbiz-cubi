8c970989f37df4b66ae5718d154d9f610728295b
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-14 13:32:47 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2986 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/service/utilService.php
+++ cubi/modules/service/utilService.php
@@ -6,5 +6,19 @@ class utilService
 	    for ($i = 0; $size >= 1024 && $i < 4; $i++) $size /= 1024;
 	    return round($size, 2).$units[$i];	 
 	}
+	
+	public static function getViewURL($viewName)
+	{
+		$urlArr = explode(".",$viewName);
+		$view = str_replace("View", "", $urlArr[2]);		
+		preg_match_all("/([A-Z]{1}[a-z]+)/s",$view,$match);
+		foreach($match[0] as $view_part)
+		{
+			$view_url .= strtolower($view_part).'_';
+		}
+		$view_url = substr($view_url,0,strlen($view_url)-1);
+		$url = $urlArr[0].'/'.$view_url;
+		return $url;
+	}
 }
 ?>
