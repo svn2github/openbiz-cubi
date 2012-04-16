c7579d22875414ab2f922515ce4ca4613ad2e09e
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-16 11:31:33 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2998 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/builder/ConfDataTableWizardForm.php
+++ cubi/modules/appbuilder/builder/ConfDataTableWizardForm.php
@@ -6,7 +6,30 @@ class ConfDataTableWizardForm extends EasyFormWizard
 	{
 		$dbConnForm = BizSystem::getObject("appbuilder.builder.ConfDBConnWizardForm");
 		$dbRec = $dbConnForm->getActiveRecord();
-		//var_dump($dbRec);
+		$dbName = $dbRec['NAME'];
+		$db = BizSystem::instance()->getDBConnection($dbname);
+    	$tblCols = $db->listTables();
+    	
+		try
+    	{
+    		$tableInfos = $db->fetchAssoc("SHOW TABLE STATUS");
+    		foreach($tableInfos as $tableInfo)
+	    	{
+	    		$result[] = $tableInfo;
+	    	}
+    	}
+    	catch(Exception $e){
+	    	foreach($tblCols as $tableName)
+	    	{
+	    		$tableInfo=array(
+	    			"Name"=>$tableName,
+	    		);
+	    		$result[] = $tableInfo;
+	    	}	
+    	}
+    	
+    	
+    	return $result;
 	}
 	
 }
