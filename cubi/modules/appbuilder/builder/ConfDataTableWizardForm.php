30edf6c538f00678a4cc5daf1c004fc6219bf44a
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-16 11:43:31 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@2999 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/builder/ConfDataTableWizardForm.php
+++ cubi/modules/appbuilder/builder/ConfDataTableWizardForm.php
@@ -1,7 +1,42 @@
 <?php 
 class ConfDataTableWizardForm extends EasyFormWizard
 {
+
+	public function getActiveRecord($recId=null)
+    {
+        if ($this->m_ActiveRecord != null)
+        {
+            if($this->m_ActiveRecord['Id'] != null)
+            {
+                return $this->m_ActiveRecord;
+            }
+        }
+
+        if ($recId==null || $recId=='')
+            $recId = BizSystem::clientProxy()->getFormInputs('_selectedId');
+        if ($recId==null || $recId=='')
+            return null;
+        $this->m_RecordId = $recId;
+		$rec=array();
+        
+		$rec = $this->fetchTableInfo($recId);
+        
+		$this->m_DataPanel->setRecordArr($rec);
+        $this->m_ActiveRecord = $rec;
+        return $rec;
+    }	
 	
+    public function fetchTableInfo($tableName)
+    {
+    	$dbConnForm = BizSystem::getObject("appbuilder.builder.ConfDBConnWizardForm");
+		$dbRec = $dbConnForm->getActiveRecord();
+		$dbName = $dbRec['NAME'];
+		$db = BizSystem::instance()->getDBConnection($dbname);
+		$tableInfos = $db->fetchAssoc("SHOW TABLE STATUS WHERE Name='$tableName'");
+		return $tableInfos[0];
+    }
+    
+    
 	public function fetchDataSet()
 	{
 		$dbConnForm = BizSystem::getObject("appbuilder.builder.ConfDBConnWizardForm");
@@ -15,6 +50,7 @@ class ConfDataTableWizardForm extends EasyFormWizard
     		$tableInfos = $db->fetchAssoc("SHOW TABLE STATUS");
     		foreach($tableInfos as $tableInfo)
 	    	{
+	    		$tableInfo["Id"] = $tableInfo['Name'];
 	    		$result[] = $tableInfo;
 	    	}
     	}
@@ -24,11 +60,14 @@ class ConfDataTableWizardForm extends EasyFormWizard
 	    		$tableInfo=array(
 	    			"Name"=>$tableName,
 	    		);
+	    		$tableInfo["Id"] = $tableInfo['Name'];
 	    		$result[] = $tableInfo;
 	    	}	
     	}
     	
-    	
+		if(!$this->m_RecordId){
+				$this->m_RecordId=$result[0]["Name"];
+		}
     	return $result;
 	}
 	
