09058c8d7f7bed0dc5989f652078d392bfb3aae3
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-17 03:34:45 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3004 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/builder/ConfDataTableWizardForm.php
+++ cubi/modules/appbuilder/builder/ConfDataTableWizardForm.php
@@ -65,9 +65,17 @@ class ConfDataTableWizardForm extends EasyFormWizard
 	    	}	
     	}
     	
+    	//set default selected record
 		if(!$this->m_RecordId){
 				$this->m_RecordId=$result[0]["Name"];
 		}
+		
+		//set paging 
+		$this->m_TotalRecords = count($result);
+		$result = array_slice($result,($this->m_CurrentPage-1)*$this->m_Range,$this->m_Range);		
+        if ($this->m_Range && $this->m_Range > 0)
+            $this->m_TotalPages = ceil($this->m_TotalRecords/$this->m_Range);
+		
     	return $result;
 	}
 	
