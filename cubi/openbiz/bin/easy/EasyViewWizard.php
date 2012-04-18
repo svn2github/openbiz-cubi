b86832746865b992c85ab254917ef78712ed58ac
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-18 05:03:46 +0000

    bug fix for Cancel()
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3017 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/bin/easy/EasyViewWizard.php
+++ cubi/openbiz/bin/easy/EasyViewWizard.php
@@ -242,10 +242,12 @@ class EasyViewWizard extends EasyView
     public function cancel()
     {
         // call all step forms Cancel method
-        foreach ($this->m_FormStates as $formName=>$state)
-        {
-            if ($state['visited'])
-                BizSystem::objectFactory()->getObject($formName)->cancel();
+        if(is_array($this->m_FormStates)){
+	        foreach ($this->m_FormStates as $formName=>$state)
+	        {
+	            if ($state['visited'])
+	                BizSystem::objectFactory()->getObject($formName)->cancel();
+	        }
         }
         $this->m_DropSession = true;
     }
