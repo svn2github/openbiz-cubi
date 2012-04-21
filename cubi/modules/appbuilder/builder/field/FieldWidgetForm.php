9ab65b19aac2f676b8655493a08831f8f27ea5ef
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-21 02:42:56 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3026 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/builder/field/FieldWidgetForm.php
+++ cubi/modules/appbuilder/builder/field/FieldWidgetForm.php
@@ -1,6 +1,6 @@
 <?php 
-require_once dirname(dirname(__FILE__)).'/ConfDataTableWizardForm.php';
-class TableWidgetForm extends ConfDataTableWizardForm
+require_once dirname(dirname(__FILE__)).'/ConfDataFieldWizardForm.php';
+class FieldWidgetForm extends ConfDataFieldWizardForm
 {
 	public function fetchData()
 	{
@@ -15,7 +15,7 @@ class TableWidgetForm extends ConfDataTableWizardForm
 		if(!$name){
 			$name=BizSystem::getObject($this->m_ParentFormName)->m_RecordId;
 		}
-		$result = $this->fetchTableInfo($name);
+		$result = $this->fetchFieldInfo($name);
 		return $result;
 	}
 	
@@ -36,73 +36,7 @@ class TableWidgetForm extends ConfDataTableWizardForm
             return;
         }
 
-       $engine 		= $recArr['Engine'];
-       $tableName 	= $recArr['Name'];
-       $comment 	= $recArr['Comment'];
-
-       if(strtoupper($recArr['_field_common'])=='Y')
-       {
-       		$sql_fields_common = "
-			    `name` varchar(255) NOT NULL,
-  				`description` text  NULL,        		
-       		";
-       		$sql_key_common = ",KEY `name` (`name`)";
-       }
-
-	   if(strtoupper($recArr['_field_sort'])=='Y')
-       {
-       		$sql_fields_sort = "
-			    `sortorder` int(11) NOT NULL,       		
-       		";
-       }
-
-	   if(strtoupper($recArr['_field_status'])=='Y')
-       {
-       		$sql_fields_sort = "
-			    `status` int(2) NOT NULL,      		
-       		";
-       }       
-      
-       if(strtoupper($recArr['_field_creator'])=='Y')
-       {
-       		$sql_fields_creator = "
-			  `create_by` int(11) NOT NULL,
-			  `create_time` datetime NOT NULL,       		
-       		";
-       }
        
-	   if(strtoupper($recArr['_field_updator'])=='Y')
-       {
-       		$sql_fields_updator = "
-			    `update_by` int(11) NOT NULL,
-  				`update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,      		
-       		";
-       }   
-
-       if($recArr['_field_data_share'])
-       if(strtoupper($recArr['_field_data_share'])=='Y')
-       {
-       		$sql_fields_share = "
-			    `owner_id` int(11) default 0,
-  				`group_id` INT(11) default '1',
-  				`group_perm` INT(11) default '1',
-  				`other_perm` INT(11) default '1' ,      		
-       		";
-       }       
-       
-       $sql = "
-       CREATE TABLE IF NOT EXISTS `$tableName` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		    $sql_fields_common
-			$sql_fields_share
-		    $sql_fields_sort
-		    $sql_fields_status		  
-			$sql_fields_updator
-			$sql_fields_creator
-		  PRIMARY KEY (`id`)
-		  $sql_key_common
-		) ENGINE=$engine  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT = '$comment' ;       
-       ";
 
 	  $db = $this->_getDBConn();
 	  $db->query($sql);
