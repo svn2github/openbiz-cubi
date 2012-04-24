0c81222a8b99c381042f2525364b9b95cd5e698a
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-24 09:43:34 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3056 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/appbuilder/lib/MetadataService.php
@@ -0,0 +1,22 @@
+<?php 
+class MetadataService
+{
+	public function ListModules()
+	{
+		$mods = array();
+        $dir = MODULE_PATH;
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                $filepath = $dir.'/'.$file;
+                if (is_dir($filepath)) {
+                    $modfile = $filepath.'/mod.xml';
+                    if (file_exists($modfile))
+                        $mods[] = $file;
+                }
+            }
+            closedir($dh);
+        } 
+        return $mods;
+	}
+}
+?>
