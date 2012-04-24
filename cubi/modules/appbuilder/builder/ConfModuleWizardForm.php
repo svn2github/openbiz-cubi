6e6c84ae084bc20b202d41d96af2d8092aace80e
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-24 10:08:09 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3057 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/builder/ConfModuleWizardForm.php
+++ cubi/modules/appbuilder/builder/ConfModuleWizardForm.php
@@ -2,5 +2,40 @@
 class ConfModuleWizardForm extends EasyFormWizard
 {
 	
+	public function fetchData()    
+	{    	            
+		if ($this->m_ActiveRecord != null)
+            return $this->m_ActiveRecord;
+    	
+        if (strtoupper($this->m_FormType) == "NEW")
+            return $this->getNewRecord();
+            
+        return parent::fetchData();
+    }
+	
+	public function getNewRecord()
+	{
+		$result = array();
+		$tableName = $this->getViewObject()->getTableName();
+		$names = explode("_", $tableName);
+		if($names[1])
+		{
+			$objectName = str_replace($names[0]."_","",$tableName);			
+			$objectName = str_replace("_"," ",$objectName);
+			$objectNameSpacing = ucwords($objectName);
+			$objectName = str_replace(" ","",$objectNameSpacing);
+		}
+		else 
+		{
+			$objectName = $names[0];
+		}
+		$result['object_name'] = $objectName."DO";
+		$result['object_desc'] = $objectNameSpacing." Description";
+		$result['module_name_create'] = ucfirst($names[0]);
+		$result['extend_type_do'] = $objectName."TypeDO";
+		$result['extend_type_desc'] = $objectNameSpacing." Type Description";
+		
+		return $result;
+	}
 }
 ?>
