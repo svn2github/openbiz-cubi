ace5e62756851bfb3991e2e1fbd6c8ec2768d4f4
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-26 13:30:10 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3083 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/lib/MetadataService.php
+++ cubi/modules/appbuilder/lib/MetadataService.php
@@ -1,7 +1,7 @@
 <?php 
 class MetadataService
 {
-	public function ListModules()
+	public function listModules()
 	{
 		$mods = array();
         $dir = MODULE_PATH;
@@ -19,6 +19,41 @@ class MetadataService
         return $mods;
 	}
 	
+	public function listDataObjects($module)
+	{
+		$dir = MODULE_PATH.DIRECTORY_SEPARATOR.$module.DIRECTORY_SEPARATOR;
+		$files = $this->_glob_recursive($dir."*.xml");
+		$result = array();
+		foreach ($files as $file)
+		{
+			$file = str_replace(MODULE_PATH.DIRECTORY_SEPARATOR,"",$file);
+			if($this->getDataObjectInfo($file))
+			{
+				$result[] = $file;
+			}
+		}
+		return $result;
+	}
+	
+	public function getDataObjectInfo($file)
+	{		
+		$file = MODULE_PATH.DIRECTORY_SEPARATOR.$file;
+		if(is_file($file))
+		{
+			$xmlArr = BizSystem::getXmlArray($file);
+			foreach($xmlArr as $key=>$value)
+			{
+				if(preg_match("/BizData/si", $key, $match))
+				{					
+					$result = $xmlArr[strtoupper($key)]["ATTRIBUTES"];			
+					$result['Id'] = $result['NAME'];
+					return $result;
+				}
+			}			
+		}
+		return false;
+	}
+	
 	public function getModuleInfo($module)
 	{		
 		$modFile = MODULE_PATH.DIRECTORY_SEPARATOR.$module.DIRECTORY_SEPARATOR.'mod.xml';
@@ -30,5 +65,17 @@ class MetadataService
 		}
 		return $result;
 	}
+	
+    private function _glob_recursive($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
+       
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+        {
+            $files = array_merge($files, $this->_glob_recursive($dir.'/'.basename($pattern), $flags));
+        }
+       
+        return $files;
+    }	
 }
 ?>
--- cubi/modules/appbuilder/metaedit/DataObjectListForm.php
+++ cubi/modules/appbuilder/metaedit/DataObjectListForm.php
@@ -1,6 +1,32 @@
 <?php
 class DataObjectListForm extends EasyForm
 {
-	
+	public function fetchDataSet()
+	{
+		$module = BizSystem::getObject("appbuilder.metaedit.ModuleFilterForm")->m_RecordId;
+		
+		$svc = BizSystem::getObject("appbuilder.lib.MetadataService");
+    	$objList = $svc->listDataObjects($module);
+    	
+    	foreach($objList as $obj)
+    	{
+    		$objInfo = $svc->getDataObjectInfo($obj);
+    		$result[] = $objInfo;
+    	}
+    	
+    	//set default selected record
+		if(!$this->m_RecordId){
+				$this->m_RecordId=$result[0]["Name"];
+		}
+		//set paging 
+		$this->m_TotalRecords = count($result);
+		if(is_array($result)){
+			$result = array_slice($result,($this->m_CurrentPage-1)*$this->m_Range,$this->m_Range);
+		}		
+        if ($this->m_Range && $this->m_Range > 0)
+            $this->m_TotalPages = ceil($this->m_TotalRecords/$this->m_Range);
+		
+    	return $result;
+	}	
 }
 ?>
