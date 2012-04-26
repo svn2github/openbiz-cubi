9bc37347734427caf0aeeab3521a51df64d76ddb
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-26 14:23:52 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3085 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/metaedit/ExistingModuleListForm.php
+++ cubi/modules/appbuilder/metaedit/ExistingModuleListForm.php
@@ -16,13 +16,21 @@ class ExistingModuleListForm extends EasyForm
 		if(!$this->m_RecordId){
 				$this->m_RecordId=$result[0]["Name"];
 		}
+
 		//set paging 
 		$this->m_TotalRecords = count($result);
-		if(is_array($result)){
-			$result = array_slice($result,($this->m_CurrentPage-1)*$this->m_Range,$this->m_Range);
-		}		
+			
         if ($this->m_Range && $this->m_Range > 0)
             $this->m_TotalPages = ceil($this->m_TotalRecords/$this->m_Range);
+		
+        if($this->m_CurrentPage > $this->m_TotalPages)
+        {
+        	$this->m_CurrentPage = $this->m_TotalPages;
+        }
+            
+        if(is_array($result)){
+			$result = array_slice($result,($this->m_CurrentPage-1)*$this->m_Range,$this->m_Range);
+		}	
 		
     	return $result;
 	}
