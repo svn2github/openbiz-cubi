91fc5e645f0988bbaa011e8b2bc4f13208676205
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-04-27 15:09:55 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3104 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/appbuilder/metaedit/TemplateListForm.php
@@ -0,0 +1,20 @@
+<?php
+require_once MODULE_PATH.'/common/form/ArrayListForm.php';
+class TemplateListForm extends ArrayListForm
+{
+	public function GetRecordList()
+	{
+		$module = BizSystem::getObject("appbuilder.metaedit.ModuleFilterForm")->m_RecordId;
+		
+		$svc = BizSystem::getObject("appbuilder.lib.MetadataService");
+    	$objList = $svc->listTemplates($module);
+    	
+    	foreach($objList as $obj)
+    	{
+    		$objInfo = $svc->getDataObjectInfo($obj);
+    		$result[] = $objInfo;
+    	}
+    	return $result;
+	}	
+}
+?>
