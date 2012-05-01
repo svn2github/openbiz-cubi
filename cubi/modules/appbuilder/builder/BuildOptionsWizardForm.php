53a9c44582287a4e0b145ed5c7f01a4c7a279cc6
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-01 10:36:42 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3138 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/appbuilder/builder/BuildOptionsWizardForm.php
+++ cubi/modules/appbuilder/builder/BuildOptionsWizardForm.php
@@ -40,7 +40,28 @@ class BuildOptionsWizardForm extends EasyFormWizard
 	{		
 		$rec = $this->readInputRecord();
 		$this->m_BuildOptions = $rec;
-		//BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_DataObjFiles=array();
+				
+		$svc = BizSystem::getService("appbuilder.lib.MetaGeneratorService");
+		$dbName			= $this->getViewObject()->getDBConnName();
+		$dbTable		= $this->getViewObject()->getTableName();
+		$dbFields		= $this->getViewObject()->getFields();		
+		$configModule 	= $this->getViewObject()->getConfigModule();
+		$buildOptions 	= $this->getViewObject()->getBuildOptions();
+		
+		$svc->setDBName($dbName);
+		$svc->setDBTable($dbTable);
+		$svc->setDBFields($dbFields);
+		$svc->setConfigModule($configModule);
+		$svc->setBuildOptions($buildOptions);		
+		$generatedFiles = $svc->generate();
+				
+		BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_DataObjFiles  = $generatedFiles['DataObjFiles'];
+		BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_FormObjFiles  = $generatedFiles['FormObjFiles'];
+		BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_ViewObjFiles  = $generatedFiles['ViewObjFiles'];
+		BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_MessageFiles  = $generatedFiles['MessageFiles'];
+		BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_TemplateFiles = $generatedFiles['TemplateFiles'];
+		BizSystem::getObject("appbuilder.builder.BuildCompletedWizardForm")->m_ModXMLFile	 = $generatedFiles['ModXMLFile'];
+		
 		parent::goNext(false);
 	}	
 }
--- cubi/modules/appbuilder/lib/MetaGeneratorService.php
+++ cubi/modules/appbuilder/lib/MetaGeneratorService.php
@@ -2,5 +2,129 @@
 class MetaGeneratorService
 {
 	
+	protected $m_DBName;
+	protected $m_DBTable;
+	protected $m_DBFields;
+	protected $m_ConfigModule;
+	protected $m_BuildOptions;
+	
+	/**
+	 * 
+	 * This files will records generated files
+	 * array - DataObjFiles
+	 * array - FormObjFiles
+	 * array - ViewObjFiles
+	 * array - MessageFiles
+	 * array - TemplateFiles
+	 * string - ModXMLFile
+	 * @var array Generated Files list
+	 */
+	protected $m_GeneratedFiles;
+	
+	public function setDBName($dbName)
+	{
+		$this->m_DBName	=	$dbName;
+		return $this;
+	}
+	
+	public function setDBTable($dbTable)
+	{
+		$this->m_DBTable	=	$dbTable;
+		return $this;
+	}
+	
+	public function setDBFields($dbFields)
+	{
+		$this->m_DBFields	=	$dbFields;
+		return $this;
+	}
+	
+	public function setConfigModule($configModule)
+	{
+		$this->m_ConfigModule	=	$configModule;
+		return $this;
+	}
+
+	public function setBuildOptions($buildOptions)
+	{
+		$this->m_BuildOptions	=	$buildOptions;
+		return $this;
+	}	
+	
+	/**
+	 * 	$svc->setDBName($dbName);
+	 *  $svc->setDBTable($dbTable);
+	 *  $svc->setDBFields($dbFields);
+	 *  $svc->setConfigModule($configModule);
+	 *  $svc->setBuildOptions($buildOptions);		
+	 *  $svc->generate();
+	 * Generate MetaObject Files
+	 */
+	public function generate()
+	{
+		$this->_genDataObj();
+		$this->_genFormObj();
+		$this->_genViewObj();
+		$this->_genTemplateFiles();
+		$this->_genMessageFiles();
+		$this->_genModuleFile();
+		return $this->m_GeneratedFiles;
+	}
+	
+	protected function _genDataObj()
+	{
+		var_dump($this);exit;
+	}
+	
+	protected function _genFormObj()
+	{
+		
+	}
+	
+	protected function _genViewObj()
+	{
+		
+	}
+
+	protected function _genTemplateFiles()
+	{
+		
+	}	
+	
+	protected function _genMessageFiles()
+	{
+		
+	}	
+	
+	protected function _genModuleFile()
+	{
+		
+	}	
+
+	protected function _enableAttachmentFeature()
+	{
+		
+	}	
+	
+	protected function _enablePictureFeature()
+	{
+		
+	}	
+	
+	protected function _enableChangeLogFeature()
+	{
+		
+	}
+
+	protected function _enableGeoLocationFeature()
+	{
+		
+	}	
+
+	protected function _enableExtendFieldsFeature()
+	{
+		
+	}	
+	
 }
 ?>
