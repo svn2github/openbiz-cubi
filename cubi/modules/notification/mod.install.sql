df0c97ed142df894be15c22363292767158ec2cf
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-19 12:59:28 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3231 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/notification/mod.install.sql
+++ cubi/modules/notification/mod.install.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS `notification` (
   `type` varchar(255) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `message` text NOT NULL,
-  `goto_url` varchar(255) NOT NULL,
+  `goto_url` varchar(255) NOT NULL DEFAULT '',
   `read_state` int(11) NOT NULL,
-  `read_access` varchar(255) NOT NULL,
-  `update_access` varchar(255) NOT NULL,
+  `read_access` varchar(255) NOT NULL DEFAULT '',
+  `update_access` varchar(255) NOT NULL DEFAULT '',
   `create_time` datetime NOT NULL,
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `notification` (
 DROP TABLE IF EXISTS `notification_checker`; 
 CREATE TABLE IF NOT EXISTS `notification_checker` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `checker` varchar(255) NOT NULL,
+  `checker` varchar(255) NOT NULL DEFAULT '',
   `last_checktime` datetime NOT NULL,  
   PRIMARY KEY (`id`),
   KEY `checker` (`checker`)
