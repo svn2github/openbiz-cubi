0163f4c748a2771cacab890d28e0ea50104b901b
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-30 05:30:08 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3331 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/system/do/UserDO.php
+++ cubi/modules/system/do/UserDO.php
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
  * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
  * @link      http://code.google.com/p/openbiz-cubi/
- * @version   $Id:$
+ * @version   $Id$
  */
  
 class UserDO extends BizDataObj
