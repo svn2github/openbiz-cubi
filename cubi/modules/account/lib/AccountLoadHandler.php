fac532916e2103860d89cf37d727050fb3b64c5a
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-31 10:54:58 +0000

    CRM Account Module Init version
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3378 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/account/lib/AccountLoadHandler.php
@@ -0,0 +1,14 @@
+<?php
+include_once (MODULE_PATH."/system/lib/ModuleLoadHandler.php");
+
+class AccountLoadHandler implements ModuleLoadHandler
+{
+    public function beforeLoadingModule($moduelLoader)
+    {
+    }
+    
+    public function postLoadingModule($moduelLoader)
+    {    	    	
+    }
+}
+?>
