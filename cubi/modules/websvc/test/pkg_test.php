eb1f21cc3ef9e9449911261cc66a9a5c2203e966
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-31 06:23:51 +0000

    add headers in xml and php files svn keyword, Id
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3376 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/websvc/test/pkg_test.php
+++ cubi/modules/websvc/test/pkg_test.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   cubi.websvc.test
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id$
+ */
+
 
 /* 
  * Test web service API
