6ccd5cece0057eeb92355792e985d0d2323b9c3e
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-31 06:07:55 +0000

    add headers in xml and php files svn keyword, Id
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3365 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/myaccount/view/DashboardView.php
+++ cubi/modules/myaccount/view/DashboardView.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   cubi.myaccount.view
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id$
+ */
+
 class DashboardView extends EasyView
 {
 	private $m_UserWidgetDO = "myaccount.do.UserWidgetDO";	
