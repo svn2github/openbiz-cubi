1dc350f01ae8c66396c188c4b47928841eea1aee
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-05-31 05:39:37 +0000

    add headers in xml and php files svn keyword, Id
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3354 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/changelog/form/ChangeLogNoCommentForm.php
+++ cubi/modules/changelog/form/ChangeLogNoCommentForm.php
@@ -1,4 +1,16 @@
-<?php 
+<?php
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   cubi.changelog.form
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id$
+ */
+
 include_once dirname(__FILE__).'/ChangeLogForm.php';
 class ChangeLogNoCommentForm extends ChangeLogForm
 {	
