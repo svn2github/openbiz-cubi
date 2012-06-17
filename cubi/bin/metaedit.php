f3b6a4c9b39a499388a7917b4ea15f661ba59487
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-06-17 03:38:12 +0000

    forwarding metaeditor to new appbuidler URL
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3482 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/bin/metaedit.php
+++ cubi/bin/metaedit.php
@@ -1,4 +1,25 @@
 <?php
+require_once('app_init.php');
+if (!APPBUILDER)
+{
+    echo "Sorry, AppBuilder/MetaEdit disable.";
+    exit;
+}
+
+$modsvc = BizSystem::getObject("system.lib.ModuleService");
+if(!$modsvc->isModuleInstalled('appbuilder'))
+{
+    echo "Sorry, AppBuilder is not installed.";
+    exit;	
+}
+
+$metaobj = $_GET['metaobj'];
+$url = APP_INDEX."/appbuilder/xml_edit/metaobj=".$metaobj;
+header("LOCATION: $url");
+exit;
+
+//below code are depercated
+
 ob_start();
 require_once('app_init.php');
 
