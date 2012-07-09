492d0cd2012c0b043ef6be6ca0539a9ec78bba8b
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-09 11:08:22 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3558 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/accounting/book/form/AccountingTransferForm.php
+++ apps/accounting/book/form/AccountingTransferForm.php
@@ -1,6 +1,49 @@
 <?php 
 class AccountingTransferForm extends EasyForm
 {
+	public function CommitTrans()
+	{
+		$recArr = $this->readInputRecord();
+        $this->setActiveRecord($recArr);
+        if (count($recArr) != 0){
+            	
+	        try
+	        {
+	            $this->ValidateForm();
+	        }
+	        catch (ValidationException $e)
+	        {
+	            $this->processFormObjError($e->m_Errors);
+	            return;
+	        }
 	
+	        $transDO = BizSystem::getObject("accounting.record.do.AccountingRecordDO");	 
+	        //credit from source
+	        $transRec = array(
+	        	"accountbook_id" => (int)$recArr['source_accountbook_id'],
+	        	"type_id" => (int)$recArr['type_id'],
+	        	"name" => (string)$recArr['name'],
+	        	"credit" => floatval($recArr['amount']),
+	        	"trans_date" => date("Y-m-d"),
+	        	"trans_type" => 'Credit',
+	        	"trans_proof" => 'Other',	        
+	        );
+	        $transDO->insertRecord($transRec);
+	        
+	        //debit to desc
+	        $transRec = array(
+	        	"accountbook_id" => (int)$recArr['dest_accountbook_id'],
+	        	"type_id" => (int)$recArr['type_id'],
+	        	"name" => (string)$recArr['name'],
+	        	"debit" => floatval($recArr['amount']),
+	        	"trans_date" => date("Y-m-d"),
+	        	"trans_type" => 'Debit',
+	        	"trans_proof" => 'Other',	        
+	        );
+	        $transDO->insertRecord($transRec);
+	        
+        }
+        $this->processPostAction();
+	}
 }
 ?>
