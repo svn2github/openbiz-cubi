e038e85f36071712d67b1aaec463523ee047204a
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-09 07:30:33 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3554 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- apps/accounting/book/widget/AccountingDetailWidgetForm.php
+++ apps/accounting/book/widget/AccountingDetailWidgetForm.php
@@ -2,12 +2,6 @@
 class AccountingDetailWidgetForm extends EasyForm
 {
 
-	public function fetchData()
-	{
-		$accountbook_id = (int)$_GET['aid']?(int)$_GET['aid']:(int)$_POST['aid'];
-		$rec = $this->getDataObj()->fetchOne("[Id]='$accountbook_id'");
-		$rec = $rec->toArray();
-		return $rec;	
-	}	
+
 }
 ?>
