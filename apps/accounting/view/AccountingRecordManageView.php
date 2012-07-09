0b46d623e80d1bdae7e9630421e1100c26d128dc
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-09 12:38:50 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3562 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ apps/accounting/view/AccountingRecordManageView.php
@@ -0,0 +1,14 @@
+<?php 
+class AccountingRecordManageView extends EasyView
+{
+	public function render()
+	{
+		$accountbook_id = $_GET["fld:Id"];
+		if(!$accountbook_id)
+		{
+			header("Location: ".APP_INDEX.'/accounting/accounting_book_manage');
+			exit;
+		}
+	}
+}
+?>
