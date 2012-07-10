d447e13bce00a9da29d0ff334cb48f43de32388d
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-10 12:18:48 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3591 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/theme/form/CustomLogoForm.php
+++ cubi/modules/theme/form/CustomLogoForm.php
@@ -1,6 +1,16 @@
 <?php 
 class CustomLogoForm extends EasyForm
 {
-	
+	public function UpdateLogo()
+	{
+		$recArr = $this->readInputRecord();
+		$imgfile = $recArr['custom_logo'];
+		$imgfile = APP_HOME.DIRECTORY_SEPARATOR.$imgfile;
+		
+		$logofile = APP_HOME.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'cubi_logo_large.png';
+		@copy($imgfile,$logofile);
+		
+        $this->processPostAction();
+	}
 }
 ?>
