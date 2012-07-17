c93f3007ad87bb38c25840b8ca64a31539a066f8
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-17 09:06:02 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3647 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/oauth/form/OauthConnectUserForm.php
+++ cubi/modules/oauth/form/OauthConnectUserForm.php
@@ -10,5 +10,14 @@ class OauthConnectUserForm extends EasyForm
 	{
 		
 	}
+	
+	public function getNewRecord()
+	{
+		$record= array(
+		"username"=>'Test User',
+		"email" =>'test Email'
+		);
+		return $record;
+	}
 }
 ?>
