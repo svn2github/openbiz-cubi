bc3d5f0b4c0aa9931a8c871542bc6724a1f5bbdd
fsliit@gmail.com <fsliit@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-20 02:28:08 +0000

    Qzone用户登录
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3716 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/oauth/libs/oauth.class.php
+++ cubi/modules/oauth/libs/oauth.class.php
@@ -106,7 +106,7 @@ class oauthClass extends EasyForm
 			 $UserOAuthArr['oauth_token']=$oauth_data['oauth_token'];
 			 $UserOAuthArr['oauth_token_secret']=$oauth_data['oauth_token_secret'];
 			 $UserOAuthArr['oauth_rawdata']=$oauth_data['access_token_json'];
-			// $UserOAuthArr['oauth_user_info']=serialize($oauth_data);
+			 $UserOAuthArr['oauth_user_info']=serialize($oauth_data);
 		    // $dataRec = new DataRecord($UserOAuthArr, $UserTokenObj);
 			// $dataRec->id =$UserToken['Id'];
 			//$dataRec->save( ); 
@@ -114,7 +114,7 @@ class oauthClass extends EasyForm
 			$UserTokenObj->updateRecords($UserOAuthArr,"[Id]={$UserToken['Id']}"); 
 			$userObj = BizSystem::getObject('system.do.UserDO');
 			$userinfo=$userObj->fetchOne("id='".$UserToken['user_id']."'");
-		 
+		
 			$profile=BizSystem::instance()->InituserProfile($userinfo['username']);
 			//获取当前用户角色的默认页
 			$index=$profile['roles'][0];  
@@ -148,7 +148,7 @@ class oauthClass extends EasyForm
 							"oauth_token"=>$OauthUserInfo['oauth_token'],
 							"oauth_token_secret"=>$OauthUserInfo['oauth_token_secret'],
 							"oauth_rawdata"=>$OauthUserInfo['access_token_json'],
-							//"oauth_user_info"=>serialize($OauthUserInfo),
+							"oauth_user_info"=>serialize($OauthUserInfo),
 							"create_by"=>$user_id,
 							"create_time"=> date("Y-m-d H:i:s")
 						);
