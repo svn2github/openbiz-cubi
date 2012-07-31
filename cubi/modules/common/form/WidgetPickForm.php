986eb57f810141c34fd3e3931844b0f2dd9659bc
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-07-31 16:02:28 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3775 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/common/form/WidgetPickForm.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   cubi.myaccount.form
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id: WidgetPickForm.php 3365 2012-05-31 06:07:55Z rockyswen@gmail.com $
+ */
+
+class WidgetPickForm extends PickerForm
+{
+	public function PicktoParent()
+	{
+	 	if ($id==null || $id=='')
+            $id = BizSystem::clientProxy()->getFormInputs('_selectedId');
+
+        $selIds = BizSystem::clientProxy()->getFormInputs('row_selections', false);
+        if ($selIds == null)
+            $selIds[] = $id;
+                    
+        foreach ($selIds as $id)
+        {
+            $rec = $this->getDataObj()->fetchById($id);           
+			$parentForm = BizSystem::objectFactory()->getObject($this->m_ParentFormName);
+			$parentDo = $parentForm->getDataObj();
+			$user_id = BizSystem::getUserProfile("Id");
+			
+			$newRec = array(
+				"user_id" => $user_id,
+				"widget" => $rec['name'],
+				"ordering" => 10,
+				"status" => 1
+			);
+			$parentDo->insertRecord($newRec);
+        }
+        
+		
+		
+		$this->close();
+        $parentForm->rerender();
+	}
+}
+?>
---
+++ cubi/modules/common/view/DashboardView.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   cubi.myaccount.view
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id: DashboardView.php 3365 2012-05-31 06:07:55Z rockyswen@gmail.com $
+ */
+
+class DashboardView extends EasyView
+{
+	protected $m_UserWidgetDO = "myaccount.do.UserWidgetDO";	
+	
+	protected function readMetadata(&$xmlArr)
+    {
+        parent::readMetaData($xmlArr);
+		$formRefXML = $this->getFormReferences();		
+        $this->m_FormRefs = new MetaIterator($formRefXML,"FormReference",$this);
+	}
+	
+	protected function getFormReferences()
+	{
+		// get user widgets of this view
+		$user_id = BizSystem::GetUserProfile("Id");
+		$viewName = $this->m_Name;
+		$searchRule="[user_id]='$user_id' AND [view]='$viewName'";
+		$do = BizSystem::GetObject($this->m_UserWidgetDO);
+		$formRecs = $do->directfetch($searchRule);
+		// if no user widgets found, get system widgets of this view
+		if (count($formRecs)==0) {
+			$searchRule="[user_id]=0 AND [view]='$viewName'";
+			$formRecs = $do->directfetch($searchRule);
+		}
+		$formRefXML = array();
+		foreach($formRecs as $form){
+			$formRefXML[] = array(
+				"ATTRIBUTES"=>array(
+					"NAME"=>$form['widget']
+					),
+				"VALUE"=>null
+			);
+		}
+
+		return $formRefXML;
+	}
+	
+	public function loadWidgetPicker()
+	{
+		$widgetPickerForm = "myaccount.form.WidgetPickForm";
+		$formObj = BizSystem::getObject($widgetPickerForm);
+		BizSystem::clientProxy()->redrawForm("DIALOG", $formObj->render());
+	}
+}
+?>
