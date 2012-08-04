8e33119d82c2e4204827864816fda1538e3fcc83
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-08-04 13:58:33 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@3807 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/theme/form/CustomHeaderForm.php
@@ -0,0 +1,16 @@
+<?php 
+class CustomHeaderForm extends EasyForm
+{
+	public function UpdateLogo()
+	{
+		$recArr = $this->readInputRecord();
+		$imgfile = $recArr['custom_header'];
+		$imgfile = APP_HOME.DIRECTORY_SEPARATOR.$imgfile;
+		
+		$logofile = APP_HOME.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'cubi_top_header.png';
+		@copy($imgfile,$logofile);
+		
+        $this->processPostAction();
+	}
+}
+?>
