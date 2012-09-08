20fff16e170883b2146859a1c124fe27f59c31ab
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-08 12:27:49 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4133 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/sms/lib/driver/SPtclk.php
@@ -0,0 +1,96 @@
+<?php 
+require_once 'iSMS.php';
+require_once 'SPDriver.php';
+require_once dirname(dirname(__FILE__)).'/dll/emay/nusoaplib/nusoap.php';
+
+
+class SPtclk extends SPDriver implements iSMS 
+{
+	protected $m_ProviderId = 5;
+	protected $m_type = 'tclk';
+
+	protected $m_URL = "http://inolink.com/WS/linkWS.asmx/";
+		
+	public function activeService()
+	{
+		$providerInfo = $this->_getProviderInfo();
+		$CorpID = $providerInfo['username'];
+		$Pwd = $providerInfo['password'];
+		
+		$CropName 	= urlencode(BizSystem::GetUserProfile("profile_company"));
+		$LinkMan 	= urlencode(BizSystem::GetUserProfile("profile_display_name"));
+		$Tel 		= urlencode(BizSystem::GetUserProfile("profile_phone"));
+		$Mobile 	= urlencode(BizSystem::GetUserProfile("profile_mobile"));
+		$Email		= urlencode(BizSystem::GetUserProfile("email"));
+				
+		$url = $this->m_URL."Reg?CorpID=$CorpID&Pwd=$Pwd&CorpName=$CorpName&LinkMan=$LinkMan&Tel=$Tel&Mobile=$Mobile&Email=$Email";
+		$result = file_get_contents($url);
+
+		if(0 == $result)
+		{
+			return true;
+		}
+		else
+		{
+		    return false;
+		}
+		
+	}
+	
+	public function send($mobile,$content,$schedule=null)
+	{		
+
+		$providerInfo  = $this->_getProviderInfo();
+		$CorpID = $providerInfo['username'];
+		$Pwd = $providerInfo['password'];
+		
+		if($schedule=="0000-00-00 00:00:00")
+		{
+			$schedule='';
+		}else{
+			$schedule = date('YmdHis',strtotime($schedule));
+		}
+		$mobile  = urlencode($mobile);
+		$content = urlencode(iconv("UTF-8","GBK",$content));
+		
+		$url = $this->m_URL."BatchSend?CorpID=$CorpID&Pwd=$Pwd&Mobile=$mobile&Content=$content&Cell=&SendTime=$schedule";
+		$result = file_get_contents($url);	
+
+		preg_match("/\">(.*?)<\/int/si", $result,$match);
+		$result = (int)$match[1];
+				
+		if($result<0)
+		{				
+			BizSystem::getService(LOG_SERVICE)->log(LOG_ERR,"SMS","sendMessage: ". $content." TCLK：".$mobile.':'.$result['msg']);
+			return false;
+		}
+		else
+		{			
+			$this->HitMessageCounter();
+			$this->_log($mobile,$content,$schedule);	
+			return true;
+		}
+			
+	}
+
+    public function getMsgBalance()
+    {       
+    	$providerInfo  = $this->_getProviderInfo();
+		$CorpID = $providerInfo['username'];
+		$Pwd = $providerInfo['password'];
+		
+		$url = $this->m_URL."SelSum?CorpID=$CorpID&Pwd=$Pwd";
+		$result = file_get_contents($url);
+			
+		preg_match("/\">(.*?)<\/int/si", $result,$match);
+		$balance = (int)$match[1];
+    	$this->updateMsgBalance($balance);
+		return $balance;
+	}
+    	
+  
+}
+
+
+
+?>
