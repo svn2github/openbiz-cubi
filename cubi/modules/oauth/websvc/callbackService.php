33859f790f6f9aa20ade508517fe956915fbf94c
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-10 08:54:18 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4153 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/oauth/websvc/callbackService.php
+++ cubi/modules/oauth/websvc/callbackService.php
@@ -3,5 +3,48 @@ require_once MODULE_PATH.'/websvc/lib/WebsvcService.php';
 class callbackService extends  WebsvcService
 {
 	
+	public function __call($method,$arguments=null)
+	{		
+		$type=BizSystem::ClientProxy()->getRequestParam("type");  
+		
+		$redirectURL=BizSystem::ClientProxy()->getRequestParam("redirect_url");
+		if($redirectURL)
+		{
+			BizSystem::sessionContext()->setVar("oauth_redirect_url", $redirectURL);
+		}
+		
+		$assocURL	=BizSystem::ClientProxy()->getRequestParam("assoc_url");
+		if($assocURL)
+		{
+			BizSystem::sessionContext()->setVar("oauth_assoc_url", $assocURL);
+		}
+		
+		$whitelist_arr = BizSystem::getService(LOV_SERVICE)->getDictionary("oauth.lov.ProviderLOV(Provider)");
+		
+		if(!in_array($type,$whitelist_arr)){
+			throw new Exception('Unknown service');
+			return;
+		}
+		 
+		$oatuthType=MODULE_PATH."/oauth/libs/{$type}.class.php";
+		if(!file_exists($oatuthType))
+		{
+			throw new Exception('Unknown type');
+			return;
+		}
+		
+		include_once $oatuthType;
+		$obj = new $type;
+		switch(strtolower($method))
+		{
+			case "callback":
+			case "login":
+				break;
+			default:
+				throw new Exception('Unknown service');
+				break;
+		}		
+		return call_user_func(array($obj,$method));				
+	}
 }
 ?>
