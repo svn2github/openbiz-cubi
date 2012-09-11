b4ce152caef8a7887a2fd11ffce2768003b5171c
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-11 05:36:03 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4169 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/payment/mod.install.sql
+++ cubi/modules/payment/mod.install.sql
@@ -1,15 +1,3 @@
-DROP TABLE IF EXISTS `payment_log`;
-CREATE TABLE IF NOT EXISTS `payment_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `provider_id` int(11) NOT NULL,
-  `mobile` varchar(11) NOT NULL,
-  `content` longtext NOT NULL,
-  `schedule` datetime NOT NULL,
-  `sent_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
-
 DROP TABLE IF EXISTS `payment_provider`;
 CREATE TABLE IF NOT EXISTS `payment_provider` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -19,15 +7,26 @@ CREATE TABLE IF NOT EXISTS `payment_provider` (
   `password` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `site_url` varchar(255) NOT NULL,
-  `description` text,
-  `msg_sent_count` int(11) DEFAULT '0' ,
-  `msg_balance` int(11) DEFAULT '0' ,
-  `msg_last_sendtime` datetime NOT NULL,
-  `priority` int(2) DEFAULT '1',
+  `description` text,  
   `status` int(2) NOT NULL DEFAULT '1',
   `update_by` int(11) NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `create_by` int(11) NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `payment_log`;
+CREATE TABLE IF NOT EXISTS `payment_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `provider_id` int(11) NOT NULL,
+  `txn_id` varchar(255) NOT NULL,
+  `payer_email` varchar(255) NOT NULL,
+  `payer_id` varchar(255) NOT NULL,
+  `payment_fee` float NOT NULL,
+  `payment_gross` float NOT NULL,
+  `payment_status` varchar(255) NOT NULL,
+  `rawdata` longtext NOT NULL,
+  `create_time` datetime NOT NULL,  
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
