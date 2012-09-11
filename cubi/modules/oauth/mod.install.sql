dcd3dc7b63a1e70af5d96aa3e341477bac3a2566
jack@openbiz.me <jack@openbiz.me@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-11 07:52:52 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4172 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/oauth/mod.install.sql
+++ cubi/modules/oauth/mod.install.sql
@@ -23,7 +23,7 @@ INSERT INTO `oauth_provider`
  VALUES
 (1, 'sina', 'http://open.weibo.com/', '', '', 0, 1, '', 1, ''),
 (2, 'qq', 'http://dev.t.qq.com/','', '', 0, 1, '', 1, ''),
-(3, 'alipay', 'https://b.alipay.com/', '', 0, 1, '', 1, ''),
+(3, 'alipay', 'https://b.alipay.com/','', '', 0, 1, '', 1, ''),
 (4, 'google', 'https://code.google.com/apis/console/','', '', 0, 1, '', 1, ''),
 (5, 'facebook', 'http://developers.facebook.com/','', '', 0, 1, '', 1, ''),
 (6, 'qzone', 'http://open.qq.com/',  '', '', 0, 1, '', 1, ''),
