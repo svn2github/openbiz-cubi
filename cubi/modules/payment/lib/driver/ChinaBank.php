bf65802b51e2ad55c4368d0a45da78ee41587ca5
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-20 05:59:51 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4250 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/payment/lib/driver/ChinaBank.php
+++ cubi/modules/payment/lib/driver/ChinaBank.php
@@ -27,7 +27,7 @@ class ChinaBank extends PaymentAdapter
 			"v_oid"		=> $orderId,
 			"v_amount"	=> $amount,
 			"v_moneytype" => $this->m_CurrencyCode,
-			"remark1"	=> $customData,					
+			"remark1"	=> base64_encode($customData),					
 		); 
 		
 		$text = $paramArr['v_amount'].$paramArr['v_moneytype'].$paramArr['v_oid'].$paramArr['v_mid'].$paramArr['v_url'].$paramArr['key'];
@@ -37,24 +37,36 @@ class ChinaBank extends PaymentAdapter
     	{
     		$url .= "$key=$value&";
     	}					
-
 		return $url;
 	}
 
 	public function GetReturnData(){
 		$data = array();		
+		$this->_convertRequestData();
 		
-		$data['buyer_account'] 	= iconv("GB2312","UTF-8",trim($_REQUEST['v_pmode']));
-		$data['buyer_id']	 	= iconv("GB2312","UTF-8",trim($_REQUEST['v_pmode']));
+		$data['buyer_account'] 	= trim($_REQUEST['v_pmode']);
+		$data['buyer_id']	 	= trim($_REQUEST['v_pmode']);
 		$data['order_id'] 		= trim($_REQUEST['v_oid']);
 		$data['trans_id'] 		= trim($_REQUEST['v_idx']);
-		$data['txn_id'] 		= trim($_REQUEST['v_oid']);
-		$data['subject'] 		= iconv("GB2312","UTF-8",trim($_REQUEST['v_pmode']));
+		$data['txn_id'] 		= trim($_REQUEST['v_idx']);
+		$data['subject'] 		= trim($_REQUEST['v_pmode']);
 		$data['amount'] 		= trim($_REQUEST['v_amount']);
 		$data['status'] 		= trim($_REQUEST['v_pstatus']);
-		$data['custom'] 		= trim($_REQUEST['remark1']);
+		$data['custom'] 		= base64_decode(trim($_REQUEST['remark1']));
 		
 		return $data;		
+	}
+	
+	protected function _convertRequestData()
+	{
+		if($_REQUEST['converted_utf8']!=1)
+		{
+			foreach($_REQUEST as $key=>$value)
+			{
+				$_REQUEST[$key] = iconv("GB2312","UTF-8",trim($value));
+			}
+			$_REQUEST['converted_utf8']=1;
+		}
 	}
 	
 	public function ValidateNotification($txn_id=null)
