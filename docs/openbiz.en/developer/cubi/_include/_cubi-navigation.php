5a88fee558ada9267e76b6872e47e32b4f476394
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-25 06:11:29 +0000

    translate cubi screenshot page
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4289 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- docs/openbiz.en/developer/cubi/_include/_cubi-navigation.php
+++ docs/openbiz.en/developer/cubi/_include/_cubi-navigation.php
@@ -6,8 +6,8 @@
 		<ul>			
 			<li><a class="introduction" 		href="./">Introduction</a></li>
 			<li><a class="rich-modules" 		href="rich-modules.php">Rich Components</a></li>
-			<li><a class="screenshot"			href="screenshot.php">Cool UI</a></li>			
-			<li><a class="create-your-brand" 	href="create-your-brand.php">Creat Your Brand</a></li>
+			<li><a class="screenshot"			href="screenshot.php">Quality UI</a></li>			
+			<li><a class="create-your-brand" 	href="create-your-brand.php">My Brand</a></li>
 			<li><a class="testimonials" 		href="testimonials.php">Testimonials</a></li>			
 			<li><a class="quick-start" 			href="quick-start.php">Quick Start</a></li>			
 		</ul>
--- docs/openbiz.en/developer/cubi/screenshot.php
+++ docs/openbiz.en/developer/cubi/screenshot.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>漂亮的用户界面 － Openbiz Cubi Platform － <?php echo SITE_NAME;?></title>
+<title>Quality UI － Openbiz Cubi Platform － <?php echo SITE_NAME;?></title>
 <link rel="icon" href="favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> 
 <link href="../../general/style/general.css" rel="stylesheet" type="text/css" />
@@ -64,10 +64,10 @@ $(document).ready(function(){
 				<div class="desc">
 					<h1 style="height:auto;"><a href="cubi/"><img src="image/screenshot/banner-title.png" title="Openbiz Cubi Platform"/></a></h1>
 					<div style="padding-left:5px;">
-					<h2 >给客户留下专业清爽之感</h2>
-					<p>我们关注了让用户感受到的每一个小细节，<br/>让您在客户面前留下一个专业高品质的印象。</p>
+					<h2 >Impressive professional design</h2>
+					<p>Openbiz Cubi tunes look and feel on each detail <br/>You have built-in high quality user interface</p>
 					<p class="buttons">
-						<a class="blue-button-go" href="#" >免费获取</a>
+						<a class="blue-button-go" href="#" >Download</a>
 					</p>
 					</div>
 				</div>
@@ -78,114 +78,114 @@ $(document).ready(function(){
 	
 	<div class="content">
 		<div class="page-splitter"></div>	
-		<h2>基础功能样式</h2>
-		<p>快速了解一下基于Openbiz Cubi平台创建的应用程序究竟是什么样子。</p>
+		<h2>Basic UI Styles</h2>
+		<p>Let's have a quick look at the look and feel of a typical Openbiz Cubi application.</p>
 		<table class="screenshots">
 			<tr>
 				<td>
-					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-1-large.png" title="Openbiz Cubi - 系统管理员截面">
+					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-1-large.png" title="Openbiz Cubi - application administration page">
 					<img src="image/screenshot/screenshot-basic-1.png" />
-					<span>系统管理员界面</span>
+					<span>Administration page</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-2-large.png" title="Openbiz Cubi - 数据列表视图">
+					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-2-large.png" title="Openbiz Cubi - data listing view">
 					<img src="image/screenshot/screenshot-basic-2.png" />
-					<span>数据列表界面</span>
+					<span>Data listing page</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-3-large.png" title="Openbiz Cubi - 数据详情视图">
+					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-3-large.png" title="Openbiz Cubi - data detail view">
 					<img src="image/screenshot/screenshot-basic-3.png" />
-					<span>数据详情界面</span>
+					<span>Data detail page</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-4-large.png" title="Openbiz Cubi - 数据编辑视图">
+					<a rel="screenshots-basic" href="../cubi/image/screenshot-intro-4-large.png" title="Openbiz Cubi - data editing view">
 					<img src="image/screenshot/screenshot-basic-4.png" />
-					<span>数据编辑界面</span>
+					<span>Data editing page</span>
 					</a>
 				</td>
 			</tr>
 		</table>
-		<p>这些界面将是我们每天都与之打交道的界面，它有自己统一的风格和使用习惯，这样的设计是为了让用户可以快速上手一个新的应用。因为每个应用的界面和操作习惯都是尽可能保持一致，不会让您的用户觉得无从下手。</p>
+		<p>These pages are interfaces that end users work on everyday. The same look and feel across all pages can keep your applications with consistant user experience. Users will work more efficiently without questions and confusions.</p>
 		
 		<div class="page-splitter"></div>
-		<h2>高级特性样式</h2>
-		<p>除了基本风格统一的基础特性之外，Openbiz Cubi 应用平台还为扩展应用领域支持了更多高级展现逻辑。<br/>
-		这将会成为您的应用系统的绝对亮点，让您的应用限的格外专业。</p>
+		<h2>Advanced UI Styles</h2>
+		<p>Basides consistant basic page styles, Openbiz Cubi supports a lot of advanced UI elements at platform level. <br/>
+		These professional widgets will be the highlights of your applications.</p>
 		<table class="screenshots">
 			<tr>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-1-large.png" title="Openbiz Cubi - 地图特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-1-large.png" title="Openbiz Cubi - map">
 					<img src="image/screenshot/screenshot-advanced-1-small.png" />
-					<span>地图特性</span>
+					<span>Map</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-2-large.png" title="Openbiz Cubi - 图表特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-2-large.png" title="Openbiz Cubi - chart">
 					<img src="image/screenshot/screenshot-advanced-2-small.png" />
-					<span>图表特性</span>
+					<span>Chart</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-3-large.png" title="Openbiz Cubi - 日历特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-3-large.png" title="Openbiz Cubi - calendar">
 					<img src="image/screenshot/screenshot-advanced-3-small.png" />
-					<span>日历特性</span>
+					<span>Calendar</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-4-large.png" title="Openbiz Cubi - 甘特图特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-4-large.png" title="Openbiz Cubi - gantt chart">
 					<img src="image/screenshot/screenshot-advanced-4-small.png" />
-					<span>甘特图特性</span>
+					<span>Gantt chart</span>
 					</a>
 				</td>
 			</tr>
 			
 			<tr>	
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-5-large.png" title="Openbiz Cubi - 多步向导特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-5-large.png" title="Openbiz Cubi - wizard">
 					<img src="image/screenshot/screenshot-advanced-5-small.png" />
-					<span>多步向导特性</span>
+					<span>Multi-step wizard</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-6-large.png" title="Openbiz Cubi - 读卡器控件">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-6-large.png" title="Openbiz Cubi - card reader">
 					<img src="image/screenshot/screenshot-advanced-6-small.png" />
-					<span>读卡器控件</span>
+					<span>Card reader</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-7-large.png" title="Openbiz Cubi - 条码扫描枪控件">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-7-large.png" title="Openbiz Cubi - barcode scanner">
 					<img src="image/screenshot/screenshot-advanced-7-small.png" />
-					<span>条码扫描枪控件</span>
+					<span>Barcode scanner</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-8-large.png" title="Openbiz Cubi - 数据共享特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-8-large.png" title="Openbiz Cubi - data sharing">
 					<img src="image/screenshot/screenshot-advanced-8-small.png" />
-					<span>数据共享特性</span>
+					<span>Data sharing</span>
 					</a>
 				</td>
 			</tr>			
 			
 			<tr>	
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-9-large.png" title="Openbiz Cubi - 电子邮件集成特性">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-9-large.png" title="Openbiz Cubi - email">
 					<img src="image/screenshot/screenshot-advanced-9-small.png" />
-					<span>电子邮件集成特性</span>
+					<span>Email integration</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-10-large.png" title="Openbiz Cubi - 社交账号绑定">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-10-large.png" title="Openbiz Cubi - social network">
 					<img src="image/screenshot/screenshot-advanced-10-small.png" />
-					<span>社交账号绑定</span>
+					<span>Social accounts</span>
 					</a>
 				</td>
 				<td>
-					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-11-large.png" title="Openbiz Cubi - 用户登陆">
+					<a rel="screenshots-advanced" href="image/screenshot/screenshot-advanced-11-large.png" title="Openbiz Cubi - user login">
 					<img src="image/screenshot/screenshot-advanced-11-small.png" />
-					<span>用户登陆</span>
+					<span>User login</span>
 					</a>
 				</td>
 				<td>
@@ -195,65 +195,64 @@ $(document).ready(function(){
 		</table>
 
 		<div class="page-splitter"></div>
-		<h2>精致的细节</h2>
-		<p>这是Openbiz宣称注重用户体验的绝佳体现。
-			为了取悦您和最终用户的眼睛，我们在这几个像素的细节效果上花费了数年的精力去不断完善。这也许是您看不到，甚至不在意的，但细节所带您和用户的感受，我们相信将成为决胜未来的关键因素。 
+		<h2>Fine-Tuned Details</h2>
+		<p>Openbiz Cubi has strength on providing superb user experience. Openbiz designers and developers have spent long hours on adjusting pixels to make the perfect display effects. The efforts can help you win the trust from your clients. 
 		</p>
 		<table class="screenshots-detail">
 			<tr>
 				<td>
 					<img src="image/screenshot/screenshot-detail-1.png" />
-					<span>帮助菜单的按钮</span>
+					<span>Collapse & expend button</span>
 				</td>
 				<td>
 					<img src="image/screenshot/screenshot-detail-2.png" />
-					<span>表格按列排序</span>
+					<span>Sorting on grid</span>
 				</td>
 				<td>					
 					<img src="image/screenshot/screenshot-detail-3.png" />
-					<span>日历特性</span>				
+					<span>Auto suggestion</span>				
 				</td>
 				<td>
 					<img src="image/screenshot/screenshot-detail-4.png" />
-					<span>列表翻页细节</span>
+					<span>Paging</span>
 				</td>
 			</tr>
 			
 			<tr>
 				<td>
 					<img src="image/screenshot/screenshot-detail-5.png" />
-					<span>帮助菜单的按钮</span>
+					<span>Form icon</span>
 				</td>
 				<td>
 					<img src="image/screenshot/screenshot-detail-6.png" />
-					<span>表格按列排序</span>
+					<span>Mouse click, hover effects</span>
 				</td>
 				<td>					
 					<img src="image/screenshot/screenshot-detail-7.png" />
-					<span>日历特性</span>				
+					<span>Image dropdown list</span>				
 				</td>
 				<td>
 					<img src="image/screenshot/screenshot-detail-8.png" />
-					<span>列表翻页细节</span>
+					<span>Context menu</span>
 				</td>
 			</tr>
 			
 			<tr>
 				<td>
 					<img src="image/screenshot/screenshot-detail-9.png" />
-					<span>帮助菜单的按钮</span>
+					<span>Text input effects</span>
 				</td>
 				<td>
 					<img src="image/screenshot/screenshot-detail-10.png" />
-					<span>表格按列排序</span>
+					<span>Notification text</span>
 				</td>
 				<td>					
 					<img src="image/screenshot/screenshot-detail-11.png" />
-					<span>日历特性</span>				
+					<span>Tabs</span>				
 				</td>
 				<td>
 					<img src="image/screenshot/screenshot-detail-12.png" />
-					<span>列表翻页细节</span>
+					<span>Page loader</span>
 				</td>
 			</tr>					
 						
@@ -263,9 +262,8 @@ $(document).ready(function(){
 		<div class="bottom-info-block">
 			<table>
 				<tr>
-					<td><a class="blue-button-go" href="#" >免费获取</a></td>
-					<td><p>Openbiz Cubi的系统界面设计关注每一个小细节，给人清爽、专业、细腻之感，让您在客户面前留下的高品质的印象。<br/>
-						赶快下载Openbiz Cubi，专为企业级应用开发而设计。</p>
+					<td><a class="blue-button-go" href="#" >Download</a></td>
+					<td><p>Download Openbiz Cubi today to show the quality of your design work.</p>
 					</td>
 				</tr>
 			</table>
