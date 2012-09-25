934ce0dd757d40ccbddd468cf2a90f5fc45166f6
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-09-25 04:21:20 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4287 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/system/websvc/userService.php
@@ -0,0 +1,21 @@
+<?php 
+require_once MODULE_PATH.'/websvc/lib/WebsvcService.php';
+class userService extends  WebsvcService
+{
+	public function getStatus()
+	{
+		$result = array();
+		$userId = BizSystem::getUserProfile("Id");
+		if($userId)
+		{
+			$result['login_status'] = 1;
+		}
+		else
+		{
+			$result['login_status'] = 0;			
+		}
+		return $result;
+	}
+	
+}
+?>
