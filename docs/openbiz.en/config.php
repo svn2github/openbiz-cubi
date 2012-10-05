c0eb7201faf7ac2b329f65f4254df8eb1688f144
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-10-05 06:00:00 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4406 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- docs/openbiz.en/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('SITE_NAME',		"Openbiz Technology");
-define('SITE_URL', 		"/gcubi/docs/openbiz.en");
-define('SITE_DOMAIN', 	"");
-//define('SITE_ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
-define('SITE_ROOT_PATH', dirname(__FILE__));
-
-?>
--- docs/openbiz.en/general/_include/_site-validation.php
+++
@@ -1,6 +0,0 @@
-<!-- Qzone 网站接入验证 QQ:315824246 -->
-<meta property="qc:admins" content="351003531767056212636" />
-<!-- 新浪微博验证 pr@openbiz.me -->
-<meta property="wb:webmaster" content="8e92888fdcb24703" />
-<!-- Google Webmaster Tools -->
-<meta name="google-site-verification" content="KowC1n39PRirNZ2xVRg3crUmKQr6yLCUCAXreFfWYF8" />
