7f507f119a4a25f8b817a51dd38ef34eb7cf51d0
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-10-26 02:58:28 +0000

    Width and Height attribute supports query Simple Expression, add a Scrolling option on metadata
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4460 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/bin/easy/element/IFrameBox.php
+++ cubi/openbiz/bin/easy/element/IFrameBox.php
@@ -6,12 +6,14 @@ class IFrameBox extends Element
 	
     public $m_Link;
     public $m_Label;
+    public $m_Scrolling;
     
     protected function readMetaData(&$xmlArr)
     {
         parent::readMetaData($xmlArr);
         $this->m_Link = isset($xmlArr["ATTRIBUTES"]["LINK"]) ? $xmlArr["ATTRIBUTES"]["LINK"] : null;
-        $this->m_Label = isset($xmlArr["ATTRIBUTES"]["LABEL"]) ? $xmlArr["ATTRIBUTES"]["LABEL"] : null;                
+        $this->m_Label = isset($xmlArr["ATTRIBUTES"]["LABEL"]) ? $xmlArr["ATTRIBUTES"]["LABEL"] : null;    
+        $this->m_Scrolling = isset($xmlArr["ATTRIBUTES"]["SCROLLING"]) ? $xmlArr["ATTRIBUTES"]["SCROLLING"] : 'auto';
     }    
     protected function getLink()
     {
@@ -20,6 +22,21 @@ class IFrameBox extends Element
         $formobj = $this->getFormObj();
         return Expression::evaluateExpression($this->m_Link, $formobj);
     }
+	protected function getWidth()
+    {
+        if ($this->m_Width == null)
+            return null;
+        $formobj = $this->getFormObj();
+        return Expression::evaluateExpression($this->m_Width, $formobj);
+    }
+	protected function getHeight()
+    {
+        if ($this->m_Height == null)
+            return null;
+        $formobj = $this->getFormObj();
+        return Expression::evaluateExpression($this->m_Height, $formobj);
+    }
+    
     public function renderLabel()
     {
         return $this->m_Label;
@@ -27,9 +44,9 @@ class IFrameBox extends Element
 	public function render(){	
 		$link = $this->getLink();
 		$text = $this->getText();	
-		$height = $this->m_Height;
-		$width = $this->m_Width;
-		$sHTML = "<iframe  src=\"$link\" width=\"$width\" height=\"$height\" frameborder=\"0\" scrolling=\"auto\" >
+		$height = $this->getHeight();
+		$width = $this->getWidth();
+		$sHTML = "<iframe  src=\"$link\" width=\"$width\" height=\"$height\" frameborder=\"0\" scrolling=\"".$this->m_Scrolling."\" >
 					<p>$text</p></iframe>";        
         return $sHTML;
 		
