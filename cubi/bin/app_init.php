d37aa1d98cc8a392cd0a9b1d14b253a0c984400f
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-12-18 07:52:01 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4904 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/bin/app_init.php
+++ cubi/bin/app_init.php
@@ -62,7 +62,7 @@ if ($appPath == '/') {
 
 /* define xhprof setting */
 define('XHPROF', 0);
-define('XHPROF_ROOT', '/Users/jixian/xhprof/'); /*Paht to xhPorf libs root*/
+define('XHPROF_ROOT', '/Users/jixian/xhprof/'); /*Path to xhPorf libs root*/
 define('XHPROF_URL', 'http://localhost/xhprof/xhprof_html/index.php?source=xhprof_testing&run=');
 
 /* APP_INDEX is /a/b/index.php in case of http://host/a/b/index.php?... */
