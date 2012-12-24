2681f3ee1d9194404f77f515073ea9dae2c4f3c8
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-12-24 14:24:14 +0000

    added install.lock protect for cron.php
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4909 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/bin/cronjob/cron.php
+++ cubi/bin/cronjob/cron.php
@@ -6,12 +6,20 @@
  * #!/usr/bin/env php path is not work with cronjob, so have to replace it to an absulately path
  * like 
  */
+if (is_file(dirname(dirname(dirname(__FILE__))) . '/files/install.lock')
+        && filesize(dirname(dirname(dirname(__FILE__))) . '/files/install.lock') == 1) {
+        	
 
-include_once (dirname(dirname(__FILE__))."/app_init.php");
-include_once ("cronService.php");
+	include_once (dirname(dirname(__FILE__))."/app_init.php");
+	include_once (dirname(__FILE__)."/cronService.php");
+	
+	$cronSvc = new CronService();
+	print date("m/d/Y H:i:s")." START cron processor".PHP_EOL;
+	$cronSvc->run();
+	print date("m/d/Y H:i:s")." END cron processor".PHP_EOL;
 
-$cronSvc = new CronService();
-print date("m/d/Y H:i:s")." START cron processor".PHP_EOL;
-$cronSvc->run();
-print date("m/d/Y H:i:s")." END cron processor".PHP_EOL;
+}else{
+	echo "Openbiz Cubi not installed yet.";
+	exit;
+}
 ?>
