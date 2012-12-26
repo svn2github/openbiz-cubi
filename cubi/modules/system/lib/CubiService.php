ae0aa321bd136a45b10193ffd00a8bb771ac1378
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-12-26 15:42:26 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4933 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/system/lib/CubiService.php
+++ cubi/modules/system/lib/CubiService.php
@@ -11,22 +11,84 @@
  * @version   $Id: ModuleService.php 4449 2012-10-22 06:21:42Z hellojixian@gmail.com $
  */
 
+include_once(MODULE_PATH."/common/lib/fileUtil.php");
+include_once(MODULE_PATH."/common/lib/httpClient.php");
+
 class CubiService
 {
 	const CUBI_VERSION = "3.0";
+	const UDC_SERVER = "http://dev.openbiz.cn/ws.php/udc/CollectService";
+	//const UDC_SERVER_URI = "http://local.openbiz.me/ws.php/udc/CollectService";
 	
 	public function getVersion()
 	{
 		return self::CUBI_VERSION;
 	}
 	
-	public function collectUserData($sendContact=0)
+	public function collectUserData($sendContact=1)
 	{
 		//sendContact = 0 ; don't send contact info
 		//sendContact = 1 ; send contact info
+		$contactRec = array();
+		if($sendContact)
+		{
+			$profileId = BizSystem::getUserProfile("profile_Id");
+	        $recArr = BizSystem::getObject("contact.do.ContactDO")->fetchById($profileId);
+	        $contactRec['name'] 		= $recArr['display_name'];
+	        $contactRec['company'] 		= $recArr['company'];
+	        $contactRec['email'] 		= $recArr['email'];
+	        $contactRec['mobile'] 		= $recArr['mobile'];
+	        $contactRec['phone'] 		= $recArr['phone'];
+		}
+		$system_uuid 	= $this->getSystemUUID();
+		$system_name 	= DEFAULT_SYSTEM_NAME;
+		$system_language = DEFAULT_LANGUAGE;
+		$system_url		= SITE_URL;		
+		$system_cubi_ver	= self::getVersion();
+		$system_openbiz_ver	= BizSystem::getVersion();		
+		$system_port	= $_SERVER['SERVER_PORT'];
+		$system_admin	= $_SERVER['SERVER_ADMIN'];
+		$internal_ip_address = $_SERVER['SERVER_ADDR'];
 		
-		$uuid = $this->getSystemUUID();
+		if(function_exists("ioncube_server_data")){
+			$server_data = ioncube_server_data();
+		}else{
+			$server_data = "";
+		}
+		
+		$systemRec = array(
+			"internal_ipaddr" => $internal_ip_address,
+			"language" => $system_language,
+			"system_name" => $system_name,
+			"system_uuid" => $system_uuid,
+			"system_url"  => $system_url,
+			"system_admin" => $system_admin,
+			"system_port" => $system_port,			
+			"system_cubi_ver" => $system_cubi_ver,
+			"system_openbiz_ver" => $system_openbiz_ver,			
+			"system_server_data" => $server_data,
+		);
+		
+		
+		$method = "collectUserData";
+		$params = array(
+			"contact_data" => $contactRec,
+			"system_data" => $systemRec
+		);
+		$argsJson = json_encode($params);
+        $query = array(	"method=$method","format=json","argsJson=$argsJson");
+		        
+		$httpClient = new HttpClient('POST');
+		foreach ($query as $q)
+		        $httpClient->addQuery($q);
+		$headerList = array();
+		$out = $httpClient->fetchContents(self::UDC_SERVER_URI, $headerList);		        
+		$cats = json_decode($out, true);
+		$result = $cats['data'];
+		return $result;
 	}
+	
+
 	
 	public function getSystemUUID()
 	{		
@@ -38,7 +100,7 @@ class CubiService
 		}
 		else
 		{
-			$uuid = uniqid();
+			$uuid = uniqid('openbiz-cubi-');
 			file_put_contents($dataFile,$uuid);	
 		}
 		return $uuid;
