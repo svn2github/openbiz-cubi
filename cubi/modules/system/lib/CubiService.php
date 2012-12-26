f907bc0f3ee97331fe9d5d8c290df44f400169b9
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-12-26 13:02:38 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4926 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/system/lib/CubiService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   cubi.system.lib
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id: ModuleService.php 4449 2012-10-22 06:21:42Z hellojixian@gmail.com $
+ */
+
+class CubiService
+{
+	public function getVersion()
+	{
+		return "3.0";
+	}
+}
+?>
