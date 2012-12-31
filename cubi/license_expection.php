cbe8cf35993582a5364e4d2949c5724981af56b5
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2012-12-31 09:03:21 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@4997 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/license_expection.php
+++ cubi/license_expection.php
@@ -33,21 +33,22 @@ function ioncube_event_handler($err_code, $params)
 	}
 	
 	$lic_file = MODULE_PATH.DIRECTORY_SEPARATOR.$moduleName.DIRECTORY_SEPARATOR.'license.key';
-	if(is_file($lic_file) && $rh_func)
+	if(!is_file($lic_file) && $rh_func)
 	{
-		$formObj = BizSystem::getObject("common.form.LicenseInvalidForm");
-		$formObj->m_SourceURL = $_SERVER['REQUEST_URI'];
-		$formObj->m_ErrorCode = $err_code;
-		$formObj->m_ErrorParams = $params;
-		$viewObj = BizSystem::getObject("common.view.LicenseInvalidView");
-		$viewObj->render();		
-	}else{
 		$formObj = BizSystem::getObject("common.form.LicenseInitializeForm");
 		$formObj->m_SourceURL = $_SERVER['REQUEST_URI'];
 		$formObj->m_ErrorCode = $err_code;
 		$formObj->m_ErrorParams = $params;		
 		$viewObj = BizSystem::getObject("common.view.LicenseInitializeView");
 		$viewObj->render();
+	}else{
+		$formObj = BizSystem::getObject("common.form.LicenseInvalidForm");
+		$formObj->m_SourceURL = $_SERVER['REQUEST_URI'];
+		$formObj->m_ErrorCode = $err_code;
+		$formObj->m_ErrorParams = $params;
+		$viewObj = BizSystem::getObject("common.view.LicenseInvalidView");
+		$viewObj->render();	
+
 	}	
 	
 	exit;
