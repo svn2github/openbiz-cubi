8a4a4d174b5fb60706e5cf5bc228fdc5889d93b9
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-01-04 11:31:38 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5043 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/modules/repository/setting/form/SettingTranslateForm.php
@@ -0,0 +1,129 @@
+<?php 
+require_once "SettingForm.php";
+class SettingTranslateForm extends SettingForm
+{
+	protected $m_TranslateDO = "repository.category.do.CategoryTranslateDO";
+	protected $m_RecordFKField = "repo_cat_id";
+	
+	
+	public function fetchData()
+	{
+		
+		$this->m_ActiveRecord = null;
+		$resultRaw = parent::fetchData();
+		foreach($resultRaw as $key=>$value)
+		{
+			if(substr($key,0,1)=='_')
+			{
+				$key = substr($key,1);
+			}
+			$result[$key]=$value;
+		}
+		
+		$lang = BizSystem::ClientProxy()->getFormInputs("fld_lang");
+		$lang?$lang:$lang=I18n::getCurrentLangCode();
+		$record_id = $result["Id"];
+		
+		$transDO = BizSystem::getObject($this->m_TranslateDO,1);
+		$currentRecord = $transDO->fetchOne("[{$this->m_RecordFKField}]='$record_id' AND [lang]='$lang'");
+		if($currentRecord){
+			$currentRecord = $currentRecord->toArray();
+			foreach($currentRecord as $field => $value)
+			{
+				$result['_'.$field]=$value;
+			}			
+		}else{
+			$result['_name'] = "";
+			$result['_description'] = "";
+		}	
+		return $result;
+	}
+	
+	public function updateRecord()
+	{
+        $currentRec = $this->fetchData();
+        $recArr = $this->readInputRecord();
+        $this->setActiveRecord($recArr);
+        if (count($recArr) != 0){
+            	
+	        try
+	        {
+	            $this->ValidateForm();
+	        }
+	        catch (ValidationException $e)
+	        {
+	            $this->processFormObjError($e->m_Errors);
+	            return;
+	        }
+	
+	        if ($this->_doUpdate($recArr, $currentRec) == false)
+	            return;
+        
+        }
+		
+		$this->m_Notices[]=$this->getMessage("TRANS_SAVED_MSG", $recArr['lang']) ;        
+		$this->rerender();
+	}
+	
+	protected function _doUpdate($inputRecord, $currentRecord)
+    {
+		
+		$lang = $inputRecord['lang'];
+		$record_id = $currentRecord["Id"];		
+		$transDO = BizSystem::getObject($this->m_TranslateDO,1);
+		
+		$newRecord = array(
+    					"{$this->m_RecordFKField}" =>$record_id,
+						"lang"=>$lang,
+						);
+		foreach($inputRecord as $field=>$value)
+		{
+			if(substr($field,0,1)=='_')
+			{
+				$newRecord[substr($field,1,strlen($field)-1)] = $value;
+			}
+		}
+		
+		$searchRule = "[{$this->m_RecordFKField}]='$record_id' AND [lang]='$lang'";
+		$currentRecord = $transDO->fetchOne($searchRule);
+		if($currentRecord){
+			$currentRecord = $currentRecord->toArray();
+		}
+		
+		
+        $dataRec = new DataRecord($currentRecord, $transDO);
+
+        foreach ($newRecord as $k => $v){
+           	$dataRec[$k] = $v; // or $dataRec->$k = $v;
+        }
+        try
+        {
+			//test dump data
+        	//var_dump($currentRecord);
+        	//var_dump($dataRec->toArray());exit;
+            $dataRec->save();
+        }
+        catch (ValidationException $e)
+        {
+            $errElements = $this->getErrorElements($e->m_Errors);           
+        	if(count($e->m_Errors)==count($errElements)){
+            	$this->processFormObjError($errElements);
+            }else{            	
+            	$errmsg = implode("<br />",$e->m_Errors);
+		        BizSystem::clientProxy()->showErrorMessage($errmsg);
+            }
+            return false;
+        }
+        catch (BDOException $e)
+        {
+            $this->processBDOException($e);
+            return false;
+        }
+		$this->m_ActiveRecord = null;
+        $this->getActiveRecord($dataRec["Id"]);
+
+        $this->runEventLog();
+        return true;
+    }
+}
+?>
