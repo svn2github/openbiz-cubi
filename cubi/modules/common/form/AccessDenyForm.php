ac7170f229a9ee2438b01c592fb3559d77640962
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-01-05 04:19:53 +0000

    bug fix for If user login and not have permission to access default page, user never can not logout. Goto login page will be redirect to default page again.
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5051 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/common/form/AccessDenyForm.php
+++ cubi/modules/common/form/AccessDenyForm.php
@@ -13,19 +13,34 @@
 
 class AccessDenyForm extends EasyForm
 {
+	public $m_isDefaultPage = false;
+	
     public function setSessionVars($sessionContext)
     {
-    	$current_url = getUrlAddress();
+    	$current_url = $this->getUrlAddress();
 		$sessionContext->setObjVar("SYSTEM", "LastViewedPage", $current_url);
 		parent::setSessionVars($sessionContext);
     }
-}
-
-function getUrlAddress()
-{
-    /*** check for https is on or not ***/
-    $url = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
-    /*** return the full address ***/
-    return $url .'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    
+    public function FetchData()
+    {
+		$url = $_SERVER['REQUEST_URI'];
+		$roleStartpages = BizSystem::getUserProfile("roleStartpage");
+		$default_url = APP_INDEX.$roleStartpages[0];		
+		if($url == $default_url)
+		{
+			$this->m_isDefaultPage = true;
+		}else{
+			$this->m_isDefaultPage = false;
+		}
+    }
+    
+	function getUrlAddress()
+	{
+	    /*** check for https is on or not ***/
+	    $url = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
+	    /*** return the full address ***/
+	    return $url .'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	}
 }
 ?>
