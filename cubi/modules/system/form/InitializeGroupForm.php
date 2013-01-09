d4fe3202e49f285ab4ac956aedf83731a030f28c
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-01-09 13:58:13 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5122 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/system/form/InitializeGroupForm.php
+++ cubi/modules/system/form/InitializeGroupForm.php
@@ -1,6 +1,63 @@
 <?php 
 class InitializeGroupForm extends EasyForm
 {
+
+	protected $m_GroupDO = "system.do.GroupDO";
 	
+	public function Initialize()
+	{
+	    $currentRec = $this->fetchData();
+        $recArr = $this->readInputRecord();
+        $this->setActiveRecord($recArr);
+        if (count($recArr) != 0){
+            	
+	        try
+	        {
+	            $this->ValidateForm();
+	        }
+	        catch (ValidationException $e)
+	        {
+	            $this->processFormObjError($e->m_Errors);
+	            return;
+	        }
+	
+	        $groupDO = BizSystem::getObject($this->m_GroupDO);
+	        //rename default group
+	        if((int)$recArr['rename_default_group']==1)
+	        {
+	        	$defaultGroupRec = $groupDO->fetchOne("","[Id] ASC");
+	        	$defaultGroupRec['name'] = $recArr['rename_default_group_name'];
+	        	$defaultGroupRec->save();
+	        }
+	        
+	        //add new groups	        
+	        foreach (array(
+	        	"add_group_1",
+	        	"add_group_2",
+	        	"add_group_3",
+	        	"add_group_4",
+	        	"add_group_5"
+	        		) as $addGroup){
+		        if((int)$recArr[$addGroup]==1)
+		        {
+		        	$groupRec = array(
+		        		"name" => $recArr[$addGroup.'_name'],
+		        		"status" => 1,
+		        	);
+		        	$groupDO->insertRecord($groupRec);
+		        }
+	        }
+	        
+	        //default data sharing setting
+	        
+	        
+	        //put init lock
+	        $group_init_lock = APP_FILE_PATH.DIRECTORY_SEPARATOR.'initialize_group.lock';
+	        file_put_contents($group_init_lock, '1');
+	        
+			//redirect back to last view
+	        
+        }		
+	}
 }
 ?>
