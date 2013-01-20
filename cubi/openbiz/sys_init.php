771b4dfca8f4166463f95019e0b73e915624f7a0
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-01-20 09:21:29 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5202 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/openbiz/sys_init.php
+++ cubi/openbiz/sys_init.php
@@ -5,7 +5,7 @@
  *
  * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
  *
- * @package   cubi.bin
+ * @package   openbiz.bin
  * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
  * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
  * @link      http://code.google.com/p/openbiz-cubi/
