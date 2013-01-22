95d50a627825fde0ef056d716a903c76da32139b
hellojixian@gmail.com <hellojixian@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-01-22 03:09:52 +0000

    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5249 104121f4-e14d-89f9-dcf5-eda1cd0138dc

---
+++ cubi/example/BizSystem.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   sample
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id: sample.php 4932 2012-12-26 15:42:10Z hellojixian@gmail.com $
+ */
+
+require_once dirname(dirname(__FILE__)).'/bin/app_init.php';
+echo "Openbiz Framework Version: ".BizSystem::getVersion();
+?>
---
+++ cubi/example/ViewObject.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Openbiz Cubi Application Platform
+ *
+ * LICENSE http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ *
+ * @package   sample
+ * @copyright Copyright (c) 2005-2011, Openbiz Technology LLC
+ * @license   http://code.google.com/p/openbiz-cubi/wiki/CubiLicense
+ * @link      http://code.google.com/p/openbiz-cubi/
+ * @version   $Id: sample.php 4932 2012-12-26 15:42:10Z hellojixian@gmail.com $
+ */
+
+require_once dirname(dirname(__FILE__)).'/bin/app_init.php';
+$objectName = "system.view.UserListView";
+$userView = BizSystem::getViewObject($objectName);	//get the view object instance
+$viewHTML = $userView->render();					//render the view to a html string
+echo $viewHTML;										//output the html string
+?>
