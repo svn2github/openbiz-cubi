843652f68d14fa32e362f270412220d68453fa24
fsliit@gmail.com <fsliit@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-01-26 14:50:02 +0000

    实现baidu ouath
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5279 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/oauth/mod.install.sql
+++ cubi/modules/oauth/mod.install.sql
@@ -28,5 +28,5 @@ INSERT INTO `oauth_provider`
 (5, 'facebook', 'https://developers.facebook.com/apps','', '', 0, 1, 1),
 (6, 'qzone', 'http://connect.qq.com',  '', '', 0, 1, 1),
 (7, 'twitter', 'http://api.twitter.com',  '', '', 0, 1, 1),
-(8, 'alitao', 'https://open.taobao.com/','', '', 0, 1, 1);
-(9, 'baiduapp', 'http://developer.baidu.com','', '', 0, 1, 1);
+(8, 'alitao', 'https://open.taobao.com/','', '', 0, 1, 1),
+(9, 'baiduapp', 'http://developer.baidu.com','', '', 0, 1, 1)
