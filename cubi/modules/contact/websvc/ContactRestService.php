ca0412de0975d3a3ce723705bcceff85a66a5e00
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-04-29 06:27:57 +0000

    sample rest service for contact module - get contact record by Id. The url is will like http://localhost/gcubi/cubi/rest.php/contact/contacts/1?format=xml http://localhost/gcubi/cubi/rest.php/contact/contacts/1?format=json
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5354 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/contact/websvc/ContactRestService.php
+++ cubi/modules/contact/websvc/ContactRestService.php
@@ -1,60 +1,45 @@
 <?php
 
-include_once MODULE_PATH.'/websvc/lib/WebsvcService.php';
-require_once 'Zend/Json.php';
+//include_once MODULE_PATH.'/websvc/lib/WebsvcService.php';
+//require_once 'Zend/Json.php';
+include_once MODULE_PATH.'/websvc/lib/Array2Xml.php';
 
 class ContactRestService
 {
     protected $errCode;
     protected $errMessage;
+	protected $resourceDOMap = array('contacts'=>'contact.do.ContactDO');
     
-    public function foo($args)
+    public function getDOName($resource)
     {
-        $resultArray['foo'] = "OK";
-		return $resultArray;
+		return $this->resourceDOMap[$resource];
     }
     
-    public function get($resource, $request, $response)
+    public function get($resource, $id, $request, $response)
     {
-        $resourceName = is_array($resource) ? $resource[0] : $resource;
-		print ">>>>> reach ContactRestService::get(). Resource Name is $resourceName. Parameters are: ";
-		print_r($request->get());
+        //print ">>>>> reach ContactRestService::get(). Resource Name is $resource. id: $id";
+		$DOName = $this->getDOName($resource);
+		if (empty($DOName)) {
+			$response->status(404);
+			$response->body("Resource '$resource' is not found.");
+			return;
+		}
+		$dataObj = BizSystem::getObject($DOName);
+		$rec = $dataObj->fetchById($id);
+		$format = strtolower($request->params('format'));
+		
+		$response->status(200);
+		if ($format == 'json') {
+			$response['Content-Type'] = 'application/json';
+			$response->body(json_encode($rec->toArray()));
+		}
+		else {
+			$response['Content-Type'] = "text/xml; charset=utf-8"; 
+			$xml = new array2xml('Data');
+			$xml->createNode($rec->toArray());
+			$response->body($xml);
+		}
 		return;
-		/*
-        $username = $this->getInput('username');
-        $api_key = $this->getInput('api_key');
-        $secret = $this->getInput('secret');
-        $format = $this->getInput('format');
-        if ($this->authenticate($username, $api_key, $secret) == false) {
-            $this->output(null, $format);
-            return;
-        }
-        */
-        $format = $this->getInput('format');
-        $service = $this->getInput('service');
-        $method = $this->getInput('method');
-        
-        // read inputs
-        $args = $this->getInputArgs('args');
-        
-        // call function
-        $response = $this->$method($args);
-                
-        $this->output($response, $format);
-    }
-
-    protected function output($response=null, $format='xml')
-    {
-        if ($this->errMessage) {
-            $errMsg = $this->errMessage;
-        }
-        else {
-            $errMsg = WebsvcError::getErrorMessage($this->errorCode);
-        }
-        $wsResp = new WebsvcResponse();
-        $wsResp->setError($this->errorCode, $errMsg);
-        $wsResp->setData($response);
-        $wsResp->output($format);
     }
 }
 
