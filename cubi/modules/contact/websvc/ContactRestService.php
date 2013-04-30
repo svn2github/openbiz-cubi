f128b9f28b8d0b8b7158cd59c4e05204e6a27083
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-04-30 07:13:43 +0000

    add GET and POST for REST API add test script
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5355 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/contact/websvc/ContactRestService.php
+++ cubi/modules/contact/websvc/ContactRestService.php
@@ -41,6 +41,53 @@ class ContactRestService
 		}
 		return;
     }
+	
+	public function post($resource, $request, $response)
+    {
+        //print ">>>>> reach ContactRestService::get(). Resource Name is $resource.";
+		$DOName = $this->getDOName($resource);
+		if (empty($DOName)) {
+			$response->status(404);
+			$response->body("Resource '$resource' is not found.");
+			return;
+		}
+		$dataObj = BizSystem::getObject($DOName);
+		$dataRec = new DataRecord(null, $dataObj);
+		$inputRecord = json_decode($request->getBody());
+        foreach ($inputRecord as $k => $v) {
+            $dataRec[$k] = $v; // or $dataRec->$k = $v;
+		}
+        try {
+           $dataRec->save();
+        }
+        catch (ValidationException $e) {
+            $response->status(400);
+			$errmsg = implode("\n",$e->m_Errors);
+			$response->body($errmsg);
+			return;
+        }
+        catch (BDOException $e) {
+            $response->status(400);
+			$errmsg = implode("\n",$e->m_Errors);
+			$response->body($errmsg);
+			return;
+        }
+		
+		$format = strtolower($request->params('format'));
+		
+		$response->status(200);
+		if ($format == 'json') {
+			$response['Content-Type'] = 'application/json';
+			$response->body(json_encode($rec->toArray()));
+		}
+		else {
+			$response['Content-Type'] = "text/xml; charset=utf-8"; 
+			$xml = new array2xml('Data');
+			$xml->createNode($dataRec->toArray());
+			$response->body($xml);
+		}
+		return;
+    }
 }
 
 ?>
---
+++ cubi/modules/contact/websvc/ContactRestTest.php
@@ -0,0 +1,36 @@
+<?php
+$cubiPath = dirname(dirname(dirname(dirname(__FILE__))));
+// defined Zend framework library home as ZEND_FRWK_HOME
+define('ZEND_FRWK_HOME', $cubiPath."/openbiz/others/");
+// add zend framework to include path
+set_include_path(get_include_path() . PATH_SEPARATOR . ZEND_FRWK_HOME);
+
+require_once 'Zend/Http/Client.php';
+
+define('REST_URL_GET', 'http://localhost/gcubi/cubi/rest.php/contact/contacts');
+define('REST_URL_POST', 'http://localhost/gcubi/cubi/rest.php/contact/contacts');
+
+class ContactRestTest
+{
+	public function testGet($id)
+	{
+		$client = new Zend_Http_Client(REST_URL_GET.'/'.$id);
+		$response = $client->request('GET');
+		print_r($response);
+	}
+	
+	public function testPost($record)
+	{
+		$client = new Zend_Http_Client(REST_URL_POST);
+		$client->setRawData(json_encode($record));
+		$response = $client->request('POST');
+		print_r($response);
+	}
+}
+
+$restTest = new ContactRestTest();
+//$restTest->testGet(1);
+
+$contactRec = array('email'=>'test@gmail.com','first_name'=>'John','last_name'=>'Smith','company'=>'ibm');
+$restTest->testPost($contactRec);
+?>
