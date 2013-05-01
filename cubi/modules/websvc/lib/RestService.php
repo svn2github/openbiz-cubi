022d686fb5efb44a0ed6afd41ec7aae2dee76888
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-05-01 07:52:43 +0000

    add query for REST API and test script
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5361 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/websvc/lib/RestService.php
+++ cubi/modules/websvc/lib/RestService.php
@@ -11,6 +11,60 @@ class RestService
     {
 		return $this->resourceDOMap[$resource];
     }
+	
+	/*
+	 * Query by page, rows, sort, sorder
+	 */
+	public function query($resource, $request, $response)
+    {
+		$DOName = $this->getDOName($resource);
+		if (empty($DOName)) {
+			$response->status(404);
+			$response->body("Resource '$resource' is not found.");
+			return;
+		}
+		// get page and sort parameters
+		$allGetVars = $request->get();
+		$queryParams = array();
+		foreach ($allGetVars as $key=>$value) {
+			if ($key == 'page' || $key == 'rows' || $key == 'sort' || $key == 'sorder' || $key == 'format') {
+				continue;
+			}
+			if ($value !== null && $value !== '') {
+				$queryParams[$key] = $value;			
+			}
+		}
+		$page = $request->params('page');
+		if (!$page) $page = 0;
+		$rows = $request->params('rows');
+		if (!$rows) $rows = 10;
+		$sort = $request->params('sort');
+		$sorder = $request->params('sorder');
+		
+		$dataObj = BizSystem::getObject($DOName);
+		//$dataObj->m_Stateless = 'N';
+		$dataObj->setQueryParameters($queryParams);
+		$dataObj->setLimit($rows, $page*$rows);
+		if ($sort && $sorder) {
+			$dataObj->setSortRule("[$sort] $sorder");
+		}
+		$dataSet = $dataObj->fetch();
+		
+		$format = strtolower($request->params('format'));
+		
+		$response->status(200);
+		if ($format == 'json') {
+			$response['Content-Type'] = 'application/json';
+			$response->body(json_encode($dataSet->toArray()));
+		}
+		else {
+			$response['Content-Type'] = "text/xml; charset=utf-8"; 
+			$xml = new array2xml('Data');
+			$xml->createNode($dataSet->toArray());
+			$response->body($xml);
+		}
+		return;
+    }
     
     public function get($resource, $id, $request, $response)
     {
