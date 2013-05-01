666d7e50d1564c9f7c4db2c7adcabc761ff475c0
rockyswen@gmail.com <rockyswen@gmail.com@104121f4-e14d-89f9-dcf5-eda1cd0138dc>
2013-05-01 05:53:56 +0000

    add PUT for REST API and test script
    git-svn-id: http://openbiz-cubi.googlecode.com/svn/trunk@5357 104121f4-e14d-89f9-dcf5-eda1cd0138dc

--- cubi/modules/contact/websvc/ContactRestService.php
+++ cubi/modules/contact/websvc/ContactRestService.php
@@ -44,7 +44,6 @@ class ContactRestService
 	
 	public function post($resource, $request, $response)
     {
-        //print ">>>>> reach ContactRestService::get(). Resource Name is $resource.";
 		$DOName = $this->getDOName($resource);
 		if (empty($DOName)) {
 			$response->status(404);
@@ -68,8 +67,7 @@ class ContactRestService
         }
         catch (BDOException $e) {
             $response->status(400);
-			$errmsg = implode("\n",$e->m_Errors);
-			$response->body($errmsg);
+			$response->body($e->getMessage());
 			return;
         }
 		
@@ -85,6 +83,55 @@ class ContactRestService
 			$xml = new array2xml('Data');
 			$xml->createNode($dataRec->toArray());
 			$response->body($xml);
+		}
+		return;
+    }
+	
+	public function put($resource, $id, $request, $response)
+    {
+		$DOName = $this->getDOName($resource);
+		if (empty($DOName)) {
+			$response->status(404);
+			$response->body("Resource '$resource' is not found.");
+			return;
+		}
+		$dataObj = BizSystem::getObject($DOName);
+		$rec = $dataObj->fetchById($id);
+		if (empty($rec)) {
+			$response->status(400);
+			$response->body("No data is found for $resource $id");
+			return;
+		}
+		$dataRec = new DataRecord($rec, $dataObj);
+		$inputRecord = json_decode($request->getBody());
+        foreach ($inputRecord as $k => $v) {
+            $dataRec[$k] = $v; // or $dataRec->$k = $v;
+		}
+        try {
+           $dataRec->save();
+        }
+        catch (ValidationException $e) {
+            $response->status(400);
+			$errmsg = implode("\n",$e->m_Errors);
+			$response->body($errmsg);
+			return;
+        }
+        catch (BDOException $e) {
+            $response->status(400);
+			$response->body($e->getMessage());
+			return;
+        }
+		
+		$format = strtolower($request->params('format'));
+		
+		$response->status(200);
+		if ($format == 'json') {
+			$response['Content-Type'] = 'application/json';
+			$response->body(json_encode($rec->toArray()));
+		}
+		else {
+			$response['Content-Type'] = "text/xml; charset=utf-8"; 
+			$response->body("Successfully update record of $resource $id");
 		}
 		return;
     }
--- cubi/modules/contact/websvc/ContactRestTest.php
+++ cubi/modules/contact/websvc/ContactRestTest.php
@@ -9,6 +9,7 @@ require_once 'Zend/Http/Client.php';
 
 define('REST_URL_GET', 'http://localhost/gcubi/cubi/rest.php/contact/contacts');
 define('REST_URL_POST', 'http://localhost/gcubi/cubi/rest.php/contact/contacts');
+define('REST_URL_PUT', 'http://localhost/gcubi/cubi/rest.php/contact/contacts');
 
 class ContactRestTest
 {
@@ -26,11 +27,22 @@ class ContactRestTest
 		$response = $client->request('POST');
 		print_r($response);
 	}
+	
+	public function testPut($record, $id)
+	{
+		$client = new Zend_Http_Client(REST_URL_PUT.'/'.$id);
+		$client->setRawData(json_encode($record));
+		$response = $client->request('PUT');
+		print_r($response);
+	}
 }
 
 $restTest = new ContactRestTest();
 //$restTest->testGet(1);
 
 $contactRec = array('email'=>'test@gmail.com','first_name'=>'John','last_name'=>'Smith','company'=>'ibm');
-$restTest->testPost($contactRec);
+//$restTest->testPost($contactRec);
+
+$contactRec = array('email'=>'test@gmail.com','first_name'=>'Steve','last_name'=>'Johnson','company'=>'ibm');
+$restTest->testPut($contactRec, 9);
 ?>
